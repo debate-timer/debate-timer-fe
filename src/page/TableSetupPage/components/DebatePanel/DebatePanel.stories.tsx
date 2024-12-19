598bea8031bb92589a89ccb157e5072bf0a82d97
jaeml06 <jaeml0630@gmail.com>
2024-12-19 19:24:14 +0900

    test: DebatePanel 스토리 북 작성

---
+++ src/page/TableSetupPage/components/DebatePanel/DebatePanel.stories.tsx
@@ -0,0 +1,55 @@
+import { Meta, StoryObj } from '@storybook/react';
+import DebatePanel from './DebatePanel';
+
+const meta: Meta<typeof DebatePanel> = {
+  title: 'page/TableSetup/Components/DebatePanel',
+  component: DebatePanel,
+  tags: ['autodocs'],
+  args: {
+    info: {
+      stance: 'PROS',
+      debateType: 'OPENING',
+      time: 150,
+      speakerNumber: 1,
+    },
+  },
+};
+
+export default meta;
+type Story = StoryObj<typeof DebatePanel>;
+
+// 찬성 입론
+export const ProsOpening: Story = {
+  args: {
+    info: {
+      stance: 'PROS',
+      debateType: 'OPENING',
+      time: 150,
+      speakerNumber: 1,
+    },
+  },
+};
+
+// 반대 반론
+export const ConsRebuttal: Story = {
+  args: {
+    info: {
+      stance: 'CONS',
+      debateType: 'REBUTTAL',
+      time: 120,
+      speakerNumber: 2,
+    },
+  },
+};
+
+// 중립 작전 시간
+export const NeutralTimeout: Story = {
+  args: {
+    info: {
+      stance: 'NEUTRAL',
+      debateType: 'TIME_OUT',
+      time: 60,
+      speakerNumber: 0,
+    },
+  },
+};
