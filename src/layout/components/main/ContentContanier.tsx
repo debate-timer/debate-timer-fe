1fe9227f86dd43ee76883710a92484c376254ca1
jaeml06 <jaeml0630@gmail.com>
2024-12-19 17:36:04 +0900

    design: layout의 ContentContanier 가운데 정렬 추가

--- src/layout/components/main/ContentContanier.tsx
+++ src/layout/components/main/ContentContanier.tsx
@@ -3,5 +3,7 @@ import { PropsWithChildren } from 'react';
 export default function ContentContanier(props: PropsWithChildren) {
   const { children } = props;
 
-  return <main className="flex flex-col">{children}</main>;
+  return (
+    <main className="flex flex-col items-center gap-4 px-8">{children}</main>
+  );
 }
