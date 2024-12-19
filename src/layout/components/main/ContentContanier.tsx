2833be3e4ccd5567f015d09ce40bb6dcb5a0c351
jaeml06 <jaeml0630@gmail.com>
2024-12-19 17:41:52 +0900

    design: layout의 ContentContanier padding 추가

--- src/layout/components/main/ContentContanier.tsx
+++ src/layout/components/main/ContentContanier.tsx
@@ -4,6 +4,8 @@ export default function ContentContanier(props: PropsWithChildren) {
   const { children } = props;
 
   return (
-    <main className="flex flex-col items-center gap-4 px-8">{children}</main>
+    <main className="flex flex-col items-center gap-4 px-8 py-4">
+      {children}
+    </main>
   );
 }
