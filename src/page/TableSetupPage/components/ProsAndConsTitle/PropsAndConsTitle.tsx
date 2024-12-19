21971a4b58bc0519b3dd4a2d5b718675d33ea73a
jaeml06 <jaeml0630@gmail.com>
2024-12-19 18:27:24 +0900

    feat: PropsAndConsTitle 구현

---
+++ src/page/TableSetupPage/components/ProsAndConsTitle/PropsAndConsTitle.tsx
@@ -0,0 +1,8 @@
+export default function PropsAndConsTitle() {
+  return (
+    <div className="mb-8 flex w-full items-center justify-around border-b border-black pb-1">
+      <span className="text-3xl font-bold text-blue-500">찬성</span>
+      <span className="text-3xl font-bold text-red-500">반대</span>
+    </div>
+  );
+}
