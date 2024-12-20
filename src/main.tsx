0a1c63c07799ebca7e2118d91811fe42cbcb1086
jaeml06 <jaeml0630@gmail.com>
2024-12-20 19:18:07 +0900

    feat: GlobalPortal 적용

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,12 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.tsx';
+import { GlobalPortal } from './util/GlobalPortal/index.ts';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <GlobalPortal.Provider>
+      <App />
+    </GlobalPortal.Provider>
   </StrictMode>,
 );
