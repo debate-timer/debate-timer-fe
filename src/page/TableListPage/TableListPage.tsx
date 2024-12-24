7172350d1b564e261deca714e9c436d605819146
eunwoo-levi <eunwoo1341@gmail.com>
2024-12-24 15:47:30 +0900

    feat: TableListPage 와이어 프레임 구현

--- src/page/TableListPage/TableListPage.tsx
+++ src/page/TableListPage/TableListPage.tsx
@@ -1,3 +1,36 @@
+import DefaultLayout from '../../layout/defaultLayout/DefaultLayout';
+
+const data = [
+  { name: '테이블1', type: '의회식 토론', time: 30 },
+  { name: '테이블 2', type: '의회식 토론', time: 30 },
+  { name: '테이블 3', type: '의회식 토론', time: 30 },
+  { name: '테이블 4', type: '의회식 토론', time: 30 },
+  { name: '테이블 5', type: '의회식 토론', time: 30 },
+];
+
+interface TableProps {
+  name: string;
+  type: string;
+  time: number;
+}
+
 export default function TableListPage() {
-  return <div>TableListPage</div>;
+  return (
+    <DefaultLayout>
+      <DefaultLayout.Header>
+        <DefaultLayout.Header.Left>테이블 목록화면</DefaultLayout.Header.Left>
+      </DefaultLayout.Header>
+      <div className="flex h-screen flex-col border-2 border-red-200 px-4 py-6">
+        <main className="grid grid-cols-3">
+          {data.map((table: TableProps, idx: number) => (
+            <section key={idx} className="w-[300px] p-2">
+              <h1>{table.name}</h1>
+              <div>유형 : {table.type}</div>
+              <div>소요시간 : {table.time}</div>
+            </section>
+          ))}
+        </main>
+      </div>
+    </DefaultLayout>
+  );
 }
