85df4caff20c61c8c4950ff919f2d086aeb64582
eunwoo-levi <eunwoo1341@gmail.com>
2024-12-24 17:09:39 +0900

    refactor: 모달 와이어프레임 구현

--- src/page/TableListPage/components/ModalToMakeTable.tsx
+++ src/page/TableListPage/components/ModalToMakeTable.tsx
@@ -20,15 +20,26 @@ export default function ModalToMakeTable({ isOpen, onClose }: ModalProps) {
   if (!isOpen) return null;
 
   return (
-    <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50">
-      <div className="relative w-[300px] rounded-md bg-white p-6">
-        <button
-          onClick={onClose}
-          className="absolute right-2 top-2 text-xl font-bold"
-        >
-          X
+    <div className="fixed inset-0 z-50 flex items-center justify-center bg-black/50">
+      <div className="relative flex h-[600px] w-[1200px] flex-col rounded-md bg-white">
+        <div className="flex h-[100px] items-center justify-center bg-neutral-300 text-5xl font-semibold">
+          <h1>어떤 토론을 원하시나요?</h1>
+          <button
+            onClick={onClose}
+            className="absolute right-1 top-1 text-xl font-bold"
+          >
+            X
+          </button>
+        </div>
+        <section className="flex-1 border-2 border-blue-100">
+          <div className="p-6">
+            <div>111</div>
+            <div>222</div>
+          </div>
+        </section>
+        <button className="h-[80px] w-full bg-red-200 text-4xl font-semibold transition duration-300 hover:bg-red-300">
+          테이블 만들기
         </button>
-        <div>테스트@@</div>
       </div>
     </div>
   );
