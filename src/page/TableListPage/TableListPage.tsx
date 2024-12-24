d576c286e2802494f61c2feb8d03dae4a86ea5f2
eunwoo-levi <eunwoo1341@gmail.com>
2024-12-24 15:56:08 +0900

    refactor: grid 가운데 정렬 및 gap 추가

--- src/page/TableListPage/TableListPage.tsx
+++ src/page/TableListPage/TableListPage.tsx
@@ -1,4 +1,5 @@
 import DefaultLayout from '../../layout/defaultLayout/DefaultLayout';
+import Table from '../LoginPage/components/Table';
 
 const data = [
   { name: '테이블1', type: '의회식 토론', time: 30 },
@@ -21,13 +22,14 @@ export default function TableListPage() {
         <DefaultLayout.Header.Left>테이블 목록화면</DefaultLayout.Header.Left>
       </DefaultLayout.Header>
       <div className="flex h-screen flex-col border-2 border-red-200 px-4 py-6">
-        <main className="grid grid-cols-3">
+        <main className="grid grid-cols-3 justify-items-center gap-4">
           {data.map((table: TableProps, idx: number) => (
-            <section key={idx} className="w-[300px] p-2">
-              <h1>{table.name}</h1>
-              <div>유형 : {table.type}</div>
-              <div>소요시간 : {table.time}</div>
-            </section>
+            <Table
+              key={idx}
+              name={table.name}
+              type={table.type}
+              time={table.time}
+            />
           ))}
         </main>
       </div>
