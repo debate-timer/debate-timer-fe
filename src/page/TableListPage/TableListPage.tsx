a6f955966e2400e80abd4968d3333b963fbc91d6
eunwoo-levi <eunwoo1341@gmail.com>
2024-12-24 16:09:33 +0900

    feat: Table 컴포넌트 구현

--- src/page/TableListPage/TableListPage.tsx
+++ src/page/TableListPage/TableListPage.tsx
@@ -1,5 +1,5 @@
 import DefaultLayout from '../../layout/defaultLayout/DefaultLayout';
-import Table from '../LoginPage/components/Table';
+import Table from './components/Table';
 
 const data = [
   { name: '테이블1', type: '의회식 토론', time: 30 },
@@ -22,7 +22,7 @@ export default function TableListPage() {
         <DefaultLayout.Header.Left>테이블 목록화면</DefaultLayout.Header.Left>
       </DefaultLayout.Header>
       <div className="flex h-screen flex-col border-2 border-red-200 px-4 py-6">
-        <main className="grid grid-cols-3 justify-items-center gap-4">
+        <main className="grid grid-cols-3 justify-items-center gap-6">
           {data.map((table: TableProps, idx: number) => (
             <Table
               key={idx}
