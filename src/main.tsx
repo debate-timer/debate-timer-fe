fe949c853924e3b91967c0b6b2ea104126b50938
jaeml06 <jaeml0630@gmail.com>
2024-12-25 21:55:33 +0900

    feat: 최상단 컴포넌트에 GlobalPortal추가

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,12 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 import { RouterProvider } from 'react-router-dom';
 import router from './route';
+import { GlobalPortal } from './util/GlobalPortal';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <GlobalPortal.Provider>
+      <RouterProvider router={router} />
+    </GlobalPortal.Provider>
   </StrictMode>,
 );
