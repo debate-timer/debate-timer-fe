448a584aa6c74ab433c80fb1dc8bdcafccdf0bf7
eunwoo-levi <eunwoo1341@gmail.com>
2024-12-25 15:18:23 +0900

    chore: Table 컴포넌트에 시간 단위 '분' 추가

--- src/page/TableListPage/components/Table.tsx
+++ src/page/TableListPage/components/Table.tsx
@@ -16,7 +16,7 @@ export default function Table({ name, type, time }: TableProps) {
       <h1 className=" text-5xl font-semibold">{name}</h1>
       <div className="flex w-full flex-grow flex-col items-start justify-center text-2xl font-semibold">
         <h1>유형 : {type}</h1>
-        <h1>소요시간 : {time}</h1>
+        <h1>소요시간 : {time}분</h1>
       </div>
     </button>
   );
