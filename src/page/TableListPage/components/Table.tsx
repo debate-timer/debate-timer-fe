2b01f4009af21be470a1ea9f4c5f0e2403229a6a
eunwoo-levi <eunwoo1341@gmail.com>
2024-12-25 13:56:18 +0900

    refactor: Table 컴포넌트 라우팅 적용

--- src/page/TableListPage/components/Table.tsx
+++ src/page/TableListPage/components/Table.tsx
@@ -1,13 +1,23 @@
+import { useNavigate } from 'react-router-dom';
 import { TableProps } from '../../../type/type';
 
 export default function Table({ name, type, time }: TableProps) {
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    navigate('/');
+  };
+
   return (
-    <section className="flex h-[200px] w-[500px] flex-col items-center rounded-md bg-amber-500 p-5 duration-200 hover:scale-105">
+    <button
+      onClick={handleClick}
+      className="flex h-[200px] w-[500px] flex-col items-center rounded-md bg-amber-500 p-5 duration-200 hover:scale-105"
+    >
       <h1 className=" text-5xl font-semibold">{name}</h1>
       <div className="flex w-full flex-grow flex-col items-start justify-center text-2xl font-semibold">
         <h1>유형 : {type}</h1>
         <h1>소요시간 : {time}</h1>
       </div>
-    </section>
+    </button>
   );
 }
