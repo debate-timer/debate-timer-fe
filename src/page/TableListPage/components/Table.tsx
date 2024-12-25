8614d625d0ae70db8d750cbcb7dc7b6a85f8b05a
eunwoo-levi <eunwoo1341@gmail.com>
2024-12-26 01:04:01 +0900

    refactor: responsive Table UI 로 리팩토링

--- src/page/TableListPage/components/Table.tsx
+++ src/page/TableListPage/components/Table.tsx
@@ -16,10 +16,10 @@ export default function Table({ name, type, time }: TableProps) {
   return (
     <button
       onClick={handleClick}
-      className="flex h-[200px] w-[500px] flex-col items-center rounded-md bg-amber-500 p-5 duration-200 hover:scale-105"
+      className="flex h-[200px] w-[200px] flex-col items-center rounded-md bg-amber-500 p-5 duration-200 hover:scale-105 md:w-[250px] lg:w-[500px]"
     >
-      <h1 className=" text-5xl font-semibold">{name}</h1>
-      <div className="flex w-full flex-grow flex-col items-start justify-center text-2xl font-semibold">
+      <h1 className="text-xl font-semibold lg:text-5xl">{name}</h1>
+      <div className="flex w-full flex-grow flex-col items-start justify-center text-lg font-semibold lg:text-2xl">
         <h1>유형 : {type}</h1>
         <h1>소요시간 : {time}분</h1>
       </div>
