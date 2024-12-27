befc9203ccdb38edf07a6fc7593485f22980d939
jaeml06 <jaeml0630@gmail.com>
2024-12-27 15:13:27 +0900

    [FIX] 타임 테이블 구성 페이지 피드백 사항 반영 (#29)
    * fix: 텍스트를 더 자세하게 수정
    
    * feat: 최상단 컴포넌트에 GlobalPortal추가
    
    * fix: 하단 버튼에 main의 content가 가려지던 문제 수정
    
    * refactor: formatSecondsToMinutes 반환 값 변경
    
    * feat: 선택 진영에 따라 모달 제목 텍스트 색상 변경
    
    * feat: input을 select로 변경
    
    * feat: stace에 따른 색상 변경 함수 구현
    
    * feat: debateType가 중립일 경우 stance을 값을 빈문자열로 표시
    
    * feat: input Numer의 leading zero삭제
    
    * feat: 초기값을 이전 설정한 값으로 설정되게 변경
    
    * feat: stace가 중립일 speakerNumber 수정 불가능하게 변경
    
    * feat: 이전 데이터가 중립일 경우 debateType이 이전 데이터를 반영하지 않도록 변경

--- src/util/formatting.ts
+++ src/util/formatting.ts
@@ -2,6 +2,6 @@ export const Formatting = {
   formatSecondsToMinutes: (time: number) => {
     const minutes = Math.floor(time / 60);
     const seconds = time % 60;
-    return `${minutes}분 ${seconds}초`;
+    return { minutes, seconds };
   },
 };
