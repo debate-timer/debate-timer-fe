ed0fb45c448fed3cc20096d423d04f7f233093ee
Shawn Kang <csm06063@naver.com>
2024-12-31 19:58:19 +0900

    chore: Added and modified codes for API
    - 가상 API 사용을 위한 msw 관련 파일 추가
    - TanStack Query 및 msw 대응하여 main.tsx 수정

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,47 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import './index.css';
 import { RouterProvider } from 'react-router-dom';
-import router from './route';
 import { GlobalPortal } from './util/GlobalPortal';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import router from './route';
+import './index.css';
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <GlobalPortal.Provider>
-      <RouterProvider router={router} />
-    </GlobalPortal.Provider>
-  </StrictMode>,
-);
+// Functions that calls msw mocking worker
+async function enableMocking() {
+  // If project is running or built on development mode, exit.
+  if (process.env.NODE_ENV !== 'development') {
+    return;
+  }
+
+  // Bring worker from specified file
+  const { worker } = await import('./mocks/browser.tsx');
+
+  // Let worker dismiss non-api calls by check whether url includes '/api/'
+  return worker.start({
+    onUnhandledRequest: (request, print) => {
+      if (!request.url.includes('/api/')) {
+        console.log(
+          "Dismissed request that doesn't include /api/: " + request.url,
+        );
+      }
+
+      print.warning();
+    },
+  });
+}
+
+enableMocking().then(() => {
+  // Call queryClient for TanStack Query
+  const queryClient = new QueryClient();
+
+  // Main React app
+  createRoot(document.getElementById('root')!).render(
+    <StrictMode>
+      <QueryClientProvider client={queryClient}>
+        <GlobalPortal.Provider>
+          <RouterProvider router={router} />
+        </GlobalPortal.Provider>
+      </QueryClientProvider>
+    </StrictMode>,
+  );
+});
