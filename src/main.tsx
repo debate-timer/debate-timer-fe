3612043711790391cd26e1050e79bbf929cc531d
Shawn Kang <77564014+i-meant-to-be@users.noreply.github.com>
2025-01-03 18:19:38 +0900

    [CHORE] API 처리를 위해 패키지 추가 (#39)
    * chore: Added API-related packages to the project
    
    * chore: Added and modified codes for API
    
    - 가상 API 사용을 위한 msw 관련 파일 추가
    - TanStack Query 및 msw 대응하여 main.tsx 수정
    
    * chore: Let msw only enabled on 'dev-mock' mode
    
    * chore: Added one blank line at end of package.json
    
    * chore: Added EOL at end of environment variable files

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,54 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import './index.css';
 import { RouterProvider } from 'react-router-dom';
-import router from './route';
 import { GlobalPortal } from './util/GlobalPortal';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import router from './route';
+import './index.css';
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <GlobalPortal.Provider>
-      <RouterProvider router={router} />
-    </GlobalPortal.Provider>
-  </StrictMode>,
-);
+// Functions that calls msw mocking worker
+if (import.meta.env.VITE_MOCK_API === 'true') {
+  console.log('[msw] Mocking enabled.');
+
+  // Import worker and start it
+  import('./mocks/browser.tsx').then(({ worker }) => {
+    worker
+      .start({
+        onUnhandledRequest: (request, print) => {
+          // Let worker dismiss non-api calls by check whether url includes '/api/'
+          if (!request.url.includes('/api/')) {
+            console.log(
+              "Dismissed request that doesn't include /api/: " + request.url,
+            );
+          }
+
+          print.warning();
+        },
+      })
+      .then(() => {
+        // After all jobs are done, initialize main React app
+        initializeApp();
+      });
+  });
+} else {
+  console.log('[msw] Mocking disabled.');
+
+  // If mocking is disabled, directly initialize main React app
+  initializeApp();
+}
+
+// Function that initializes main React app
+function initializeApp() {
+  // Call queryClient for TanStack Query
+  const queryClient = new QueryClient();
+
+  createRoot(document.getElementById('root')!).render(
+    <StrictMode>
+      <QueryClientProvider client={queryClient}>
+        <GlobalPortal.Provider>
+          <RouterProvider router={router} />
+        </GlobalPortal.Provider>
+      </QueryClientProvider>
+    </StrictMode>,
+  );
+}
