e3126a765ee2f7bf7b9485276f5dc729536dc6de
eunwoo-levi <eunwoo1341@gmail.com>
2025-01-04 03:20:47 +0900

    feat: Table 컴포넌트에 Icons 추가

--- src/page/TableListPage/components/Table/Table.tsx
+++ src/page/TableListPage/components/Table/Table.tsx
@@ -1,5 +1,6 @@
 import { useNavigate } from 'react-router-dom';
 import { DebateTable } from '../../../../type/type';
+import { AiOutlineDelete, AiOutlineEdit } from 'react-icons/ai';
 
 export default function Table({ name, type, time }: DebateTable) {
   const navigate = useNavigate();
@@ -13,6 +14,14 @@ export default function Table({ name, type, time }: DebateTable) {
       onClick={handleClick}
       className="flex h-[200px] w-11/12 flex-col items-center rounded-md bg-amber-500 p-5 duration-200 hover:scale-105"
     >
+      <div className="flex w-full justify-end gap-4 pb-2 lg:pb-0">
+        <button className="transform text-lg  duration-200 hover:scale-125 lg:text-2xl">
+          <AiOutlineEdit />
+        </button>
+        <button className="transform text-lg duration-200 hover:scale-125  lg:text-2xl">
+          <AiOutlineDelete />
+        </button>
+      </div>
       <h1 className="text-3xl font-semibold lg:text-5xl">{name}</h1>
       <div className="flex w-full flex-grow flex-col items-start justify-center text-lg font-semibold lg:text-2xl">
         <h1>유형 : {type}</h1>
