45c30decd3efb28af15581707e14ba057fb0872f
EunWoo <eunwoo1341@gmail.com>
2025-01-06 00:19:47 +0900

    [FEAT] 테이블 수정 및 삭제 구현 (#44)
    * chore: 수정 및 삭제 아이콘을 위해 react-icons 추가
    
    * feat: Table 컴포넌트에 Icons 추가
    
    * feat: implement handleDelete method
    
    * feat: implement EditModalButton to edit Tables
    
    * refactor: stopPropagation method with MouseEvent 추가
    - 버튼 클릭 이벤트가 상위로 전파되는 것을 막기 위해서 추가함
    
    * feat: Edit 버튼 눌렀을 때, CreateTableModal 나오게 구현
    
    * chore: unused closeModal function 삭제
    
    * feat: Table 삭제를 위해 DeleteModalButton 구현
    
    * feat: EditTableModal 구현
    
    * feat: EditTableButton 구현
    - 이후 수정 RestAPI 로직 추가 필요
    
    * refactor: Edit 관련 컴포넌트에 name 매개변수 추가
    
    * refactor: DebateTable 타입에 extends하여 delete 타입 추가
    
    * refactor: 토론 유형 수정 불가
    
    * refactor: 토론유형 hover: curser-not-allowed 추가
    
    * refactor: handleDelete 함수형 업데이트로 수정
    
    * refactor: EditTableButton 컴포넌트에 closeModal 매개변수 추가
    
    * fix: TableListPage 테스트코드 수정

---
+++ src/page/TableListPage/components/Modal/EditModalButton.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { AiOutlineEdit } from 'react-icons/ai';
+import { useModal } from '../../../../hooks/useModal';
+import EditTableModal from './EditTableModal';
+
+export default function EditModalButton({
+  name,
+  type,
+}: {
+  name: string;
+  type: string;
+}) {
+  const { openModal, closeModal, ModalWrapper } = useModal();
+
+  const handleEdit = (e: React.MouseEvent) => {
+    e.stopPropagation();
+    openModal();
+  };
+
+  return (
+    <>
+      <button
+        onClick={handleEdit}
+        className="transform text-lg  duration-200 hover:scale-125 lg:text-2xl"
+      >
+        <AiOutlineEdit />
+      </button>
+      <ModalWrapper>
+        <EditTableModal name={name} type={type} closeModal={closeModal} />
+      </ModalWrapper>
+    </>
+  );
+}
