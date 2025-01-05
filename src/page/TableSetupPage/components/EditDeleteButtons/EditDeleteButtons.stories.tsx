546f60fe21478009f924f09c79646d09119399f0
jaeml06 <jaeml0630@gmail.com>
2025-01-06 01:17:42 +0900

    test: EditDeleteButtons 스토리북 코드 작성

---
+++ src/page/TableSetupPage/components/EditDeleteButtons/EditDeleteButtons.stories.tsx
@@ -0,0 +1,41 @@
+import { Meta, StoryObj } from '@storybook/react';
+import EditDeleteButtons from './EditDeleteButtons';
+import { DebateInfo } from '../../../../type/type';
+import { MemoryRouter } from 'react-router-dom';
+import { GlobalPortal } from '../../../../util/GlobalPortal';
+
+const meta: Meta<typeof EditDeleteButtons> = {
+  title: 'page/TableSetup/components/EditDeleteButtons',
+  component: EditDeleteButtons,
+  tags: ['autodocs'],
+  decorators: [
+    (Story) => (
+      <MemoryRouter>
+        <GlobalPortal.Provider>
+          <div className="h-screen">
+            <Story />
+          </div>
+        </GlobalPortal.Provider>
+      </MemoryRouter>
+    ),
+  ],
+};
+
+export default meta;
+
+type Story = StoryObj<typeof EditDeleteButtons>;
+
+const mockInfo: DebateInfo = {
+  stance: 'PROS',
+  debateType: 'OPENING',
+  time: 150,
+  speakerNumber: 1,
+};
+
+export const Default: Story = {
+  args: {
+    info: mockInfo,
+    onSubmitEdit: () => {},
+    onSubmitDelete: () => {},
+  },
+};
