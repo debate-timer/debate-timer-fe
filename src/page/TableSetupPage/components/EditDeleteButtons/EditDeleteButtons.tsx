ae0a165b8a469314ca9670f4401a95011e127ac6
jaeml06 <jaeml0630@gmail.com>
2025-01-05 15:55:02 +0900

    feat: 삭제 버튼 클릭 시, 삭제 확인 모달 구현

--- src/page/TableSetupPage/components/EditDeleteButtons/EditDeleteButtons.tsx
+++ src/page/TableSetupPage/components/EditDeleteButtons/EditDeleteButtons.tsx
@@ -2,6 +2,7 @@ import { AiOutlineDelete, AiOutlineEdit } from 'react-icons/ai';
 import { useModal } from '../../../../hooks/useModal';
 import TimerCreationContent from '../TimerCreationContent/TimerCreationContent';
 import { DebateInfo } from '../../../../type/type';
+import DeleteConfirmContent from '../DeleteConfirmContent/DeleteConfirmContent';
 
 interface EditDeleteButtonsPros {
   info: DebateInfo;
@@ -10,29 +11,43 @@ interface EditDeleteButtonsPros {
 }
 
 export default function EditDeleteButtons(props: EditDeleteButtonsPros) {
-  const { openModal, closeModal, ModalWrapper } = useModal();
+  const {
+    openModal: openEditModal,
+    closeModal: closeEditModal,
+    ModalWrapper: EditModalWrapper,
+  } = useModal();
+  const {
+    openModal: deleteOpenModal,
+    closeModal: deleteCloseModal,
+    ModalWrapper: DeleteModalWrapper,
+  } = useModal();
   const { info, onSubmitEdit, onSubmitDelete } = props;
   return (
     <>
       <div className="flex w-full justify-end gap-2">
-        <button onClick={openModal}>
+        <button onClick={openEditModal}>
           <AiOutlineEdit />
         </button>
-        <button>
-          <AiOutlineDelete onClick={onSubmitDelete} />
+        <button onClick={deleteOpenModal}>
+          <AiOutlineDelete />
         </button>
       </div>
-      <ModalWrapper>
+      <EditModalWrapper>
         <TimerCreationContent
           selectedStance={info.stance}
           initDate={info}
           onSubmit={(newInfo) => {
             onSubmitEdit(newInfo);
           }}
-          onClose={closeModal}
+          onClose={closeEditModal}
         />
-        ,
-      </ModalWrapper>
+      </EditModalWrapper>
+      <DeleteModalWrapper>
+        <DeleteConfirmContent
+          onDelete={onSubmitDelete}
+          onClose={deleteCloseModal}
+        />
+      </DeleteModalWrapper>
     </>
   );
 }
