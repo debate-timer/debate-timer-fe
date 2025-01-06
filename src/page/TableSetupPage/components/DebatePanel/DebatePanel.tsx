c22bd39ecaf539c902bad0d60d6362ca11a90724
jaeml06 <jaeml0630@gmail.com>
2025-01-06 15:01:23 +0900

    [FEAT] 타임박스에 수정 및 삭제 UI 추가 (#42)
    * chore: 수정, 삭제 아이콘 이용을 위한 react-icons 추가
    
    * style: 컴포넌트 간의 간경을 더 좁게 수정
    
    * feat: 수정, 삭제 버튼을 합친 EditDeleteButtons 컴포넌트 구현
    
    * style: 분기에 따른 컴포넌트의 높이를 동일하게 수정
    
    * feat: 수정, 삭제 handler 함수 작성
    
    * refactor: 가독성을 위해 중첩된 삼항연산자 분리
    
    * feat: 삭제 버튼 클릭 시, 삭제 확인 모달 구현
    
    * feat: EditDeleteButtons에 aria-label 추가
    
    * test: EditDeleteButtons 스토리북 코드 작성
    
    * feat: TableSetup 테스트에 수정, 삭제 기능 테스트 추가

--- src/page/TableSetupPage/components/DebatePanel/DebatePanel.tsx
+++ src/page/TableSetupPage/components/DebatePanel/DebatePanel.tsx
@@ -1,47 +1,67 @@
 import { DEBATE_TYPE_LABELS, DebateInfo } from '../../../../type/type';
 import { Formatting } from '../../../../util/formatting';
+import EditDeleteButtons from '../EditDeleteButtons/EditDeleteButtons';
 
 interface DebatePanelProps {
   info: DebateInfo;
+  onSubmitEdit: (updatedInfo: DebateInfo) => void;
+  onSubmitDelete: () => void;
 }
 
-export default function DebatePanel({ info }: DebatePanelProps) {
-  const { stance, debateType, time, speakerNumber } = info;
+export default function DebatePanel(props: DebatePanelProps) {
+  const { stance, debateType, time, speakerNumber } = props.info;
+  const { onSubmitEdit, onSubmitDelete } = props;
 
   const debateTypeLabel = DEBATE_TYPE_LABELS[debateType];
+  const { minutes, seconds } = Formatting.formatSecondsToMinutes(time);
+  const timeStr = `${minutes}분 ${seconds}초`;
 
   const isPros = stance === 'PROS';
   const isCons = stance === 'CONS';
   const isNeutralTimeout = debateType === 'TIME_OUT' && stance === 'NEUTRAL';
-  const { minutes, seconds } = Formatting.formatSecondsToMinutes(time);
-  const timeStr = `${minutes}분 ${seconds}초`;
+
+  const containerClass = isPros
+    ? 'justify-start'
+    : isCons
+      ? 'justify-end'
+      : 'justify-center';
+
+  const renderProsConsPanel = () => (
+    <div
+      className={`flex w-1/2 flex-col items-center rounded-md ${
+        isPros ? 'bg-blue-500' : 'bg-red-500'
+      } h-24 p-2 font-bold text-white`}
+    >
+      <EditDeleteButtons
+        info={props.info}
+        onSubmitEdit={onSubmitEdit}
+        onSubmitDelete={onSubmitDelete}
+      />
+      <div>
+        {debateTypeLabel} / {speakerNumber}번 토론자
+      </div>
+      <div className="text-2xl">{timeStr}</div>
+    </div>
+  );
+
+  const renderNeutralTimeoutPanel = () => (
+    <div className="flex h-24 w-full items-center text-center">
+      <div className="flex h-4/5 w-full flex-col items-center justify-start rounded-md bg-gray-200 p-2 font-medium text-gray-600">
+        <EditDeleteButtons
+          info={props.info}
+          onSubmitEdit={onSubmitEdit}
+          onSubmitDelete={onSubmitDelete}
+        />
+        {debateTypeLabel} | {timeStr}
+      </div>
+    </div>
+  );
 
   return (
-    <div
-      className={`flex w-full items-center ${
-        isPros ? 'justify-start' : isCons ? 'justify-end' : 'justify-center'
-      }`}
-    >
-      {(isPros || isCons) && (
-        <div
-          className={`flex w-1/2 flex-col items-center rounded-md ${
-            isPros ? 'bg-blue-500' : 'bg-red-500'
-          } p-4 font-bold text-white`}
-        >
-          <div>
-            {debateTypeLabel} / {speakerNumber}번 토론자
-          </div>
-          <div className="text-2xl">{timeStr}</div>
-        </div>
-      )}
+    <div className={`flex w-full items-center ${containerClass}`}>
+      {(isPros || isCons) && renderProsConsPanel()}
 
-      {isNeutralTimeout && (
-        <div className="w-full rounded-md bg-gray-200 py-2 text-center">
-          <span className="font-medium text-gray-600">
-            {debateTypeLabel} | {timeStr}
-          </span>
-        </div>
-      )}
+      {isNeutralTimeout && renderNeutralTimeoutPanel()}
     </div>
   );
 }
