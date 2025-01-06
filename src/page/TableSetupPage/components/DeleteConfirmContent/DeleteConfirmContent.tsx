2d0fdcaeaf8a10bbbcee1069c36db1a381197d52
jaeml06 <jaeml0630@gmail.com>
2025-01-06 15:01:23 +0900

    [FEAT] 타임박스에 수정 및 삭제 UI 추가 (#42)
    * chore: 수정, 삭제 아이콘 이용을 위한 react-icons 추가
    
    * style: 컴포넌트 간의 간경을 더 좁게 수정
    
    * feat: 수정, 삭제 버튼을 합친 EditDeleteButtons 컴포넌트 구현
    
    * style: 분기에 따른 컴포넌트의 높이를 동일하게 수정
    
    * feat: 수정, 삭제 handler 함수 작성
    
    * refactor: 가독성을 위해 중첩된 삼항연산자 분리
    
    * feat: 삭제 버튼 클릭 시, 삭제 확인 모달 구현
    
    * feat: EditDeleteButtons에 aria-label 추가
    
    * test: EditDeleteButtons 스토리북 코드 작성
    
    * feat: TableSetup 테스트에 수정, 삭제 기능 테스트 추가

---
+++ src/page/TableSetupPage/components/DeleteConfirmContent/DeleteConfirmContent.tsx
@@ -0,0 +1,29 @@
+interface DeleteConfirmContentProps {
+  onDelete: () => void;
+  onClose: () => void; // 모달 닫기 함수
+}
+
+export default function DeleteConfirmContent({
+  onDelete,
+  onClose,
+}: DeleteConfirmContentProps) {
+  const handleDelete = () => {
+    onDelete();
+    onClose();
+  };
+  return (
+    <div className="p-10">
+      <h2 className={`mb-4 text-xl font-bold`}>
+        타임 박스를 삭제하시겠습니까?
+      </h2>
+      <div className="flex flex-col space-y-6">
+        <button
+          className="mt-4 w-full rounded bg-amber-300 p-2 hover:bg-amber-500"
+          onClick={handleDelete}
+        >
+          삭제하기
+        </button>
+      </div>
+    </div>
+  );
+}
