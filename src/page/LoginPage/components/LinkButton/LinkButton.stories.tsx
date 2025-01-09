7adf6c80973a7eff3414ca0a2f2ba1e19b7e66c7
jaeml06 <jaeml0630@gmail.com>
2025-01-09 17:25:27 +0900

    [CHORE] storybook에 전역적인 decorators 설정 추가 (#50)
    * chore: 라우터, GlobalPortal설정을 전역 설정에 decorators로 추가
    
    * chore: storybook에 msw 설정 추가

--- src/page/LoginPage/components/LinkButton/LinkButton.stories.tsx
+++ src/page/LoginPage/components/LinkButton/LinkButton.stories.tsx
@@ -1,18 +1,29 @@
 import { Meta, StoryObj } from '@storybook/react';
-import { MemoryRouter, Routes, Route, useLocation } from 'react-router-dom';
+import { Routes, Route, useLocation } from 'react-router-dom';
 import LinkButton from './LinkButton';
 
 const meta: Meta<typeof LinkButton> = {
   title: 'page/LoginPage/Components/LinkButton',
   component: LinkButton,
   tags: ['autodocs'],
+  decorators: [
+    (Story) => (
+      <>
+        <Routes>
+          <Route path="/" element={<div>Login Page</div>} />
+          <Route path="/table" element={<div>Table List Page</div>} />
+        </Routes>
+        <Story />
+        <CurrentRoute />
+      </>
+    ),
+  ],
 };
 
 export default meta;
 
 type Story = StoryObj<typeof LinkButton>;
 
-// 라우트 전환 결과를 표시하는 컴포넌트
 const CurrentRoute = () => {
   const location = useLocation();
   return <div>현재 경로: {location.pathname}</div>;
@@ -23,17 +34,4 @@ export const Default: Story = {
     url: '/table',
     title: '로그인',
   },
-
-  decorators: [
-    (Story) => (
-      <MemoryRouter initialEntries={['/']}>
-        <Routes>
-          <Route path="/" element={<div>Login Page</div>} />
-          <Route path="/table" element={<div>Table List Page</div>} />
-        </Routes>
-        <Story />
-        <CurrentRoute />
-      </MemoryRouter>
-    ),
-  ],
 };
--- src/page/TableListPage/TableListPage.stories.tsx
+++ src/page/TableListPage/TableListPage.stories.tsx
@@ -1,21 +1,10 @@
 import { Meta, StoryObj } from '@storybook/react';
 import TableListPage from './TableListPage';
-import { MemoryRouter } from 'react-router-dom';
-import { GlobalPortal } from '../../util/GlobalPortal';
 
 const meta: Meta<typeof TableListPage> = {
   title: 'page/TableListPage',
   component: TableListPage,
   tags: ['autodocs'],
-  decorators: [
-    (Story) => (
-      <MemoryRouter>
-        <GlobalPortal.Provider>
-          <Story />
-        </GlobalPortal.Provider>
-      </MemoryRouter>
-    ),
-  ],
 };
 
 export default meta;
