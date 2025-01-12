c52d2b5bb846c63fe817951d93635e15963b2aa3
jaeml06 <jaeml0630@gmail.com>
2025-01-12 23:39:06 +0900

    feat: 멀티 스텝 폼을 위해 TableSetupPage의 commponents 파일 이동

---
+++ src/page/TableComposition/components/EditDeleteButtons/EditDeleteButtons.stories.tsx
@@ -0,0 +1,28 @@
+import { Meta, StoryObj } from '@storybook/react';
+import EditDeleteButtons from './EditDeleteButtons';
+import { DebateInfo } from '../../../../type/type';
+
+const meta: Meta<typeof EditDeleteButtons> = {
+  title: 'page/TableSetup/components/EditDeleteButtons',
+  component: EditDeleteButtons,
+  tags: ['autodocs'],
+};
+
+export default meta;
+
+type Story = StoryObj<typeof EditDeleteButtons>;
+
+const mockInfo: DebateInfo = {
+  stance: 'PROS',
+  type: 'OPENING',
+  time: 150,
+  speakerNumber: 1,
+};
+
+export const Default: Story = {
+  args: {
+    info: mockInfo,
+    onSubmitEdit: () => {},
+    onSubmitDelete: () => {},
+  },
+};
---
+++ src/page/TableComposition/components/TimerCreationContent/TimerCreationContent.tsx
@@ -0,0 +1,168 @@
+import { useState } from 'react';
+import { DebateInfo, DebateType, Stance } from '../../../../type/type';
+import { Formatting } from '../../../../util/formatting';
+
+interface TimerCreationContentProps {
+  selectedStance: Stance;
+  initDate?: DebateInfo;
+  onSubmit: (data: DebateInfo) => void;
+  onClose: () => void; // 모달 닫기 함수
+}
+
+export default function TimerCreationContent({
+  selectedStance,
+  initDate,
+  onSubmit,
+  onClose,
+}: TimerCreationContentProps) {
+  const [stance, setStance] = useState<Stance>(selectedStance);
+  const [debateType, setDebateType] = useState<DebateType>(
+    initDate?.stance === 'NETURAL' ? 'OPENING' : (initDate?.type ?? 'OPENING'),
+  );
+  const { minutes: initMinutes, seconds: initSeconds } =
+    Formatting.formatSecondsToMinutes(initDate?.time ?? 180);
+
+  const [minutes, setMinutes] = useState(initMinutes);
+  const [seconds, setSeconds] = useState(initSeconds);
+  const [speakerNumber, setSpeakerNumber] = useState<number | null>(
+    initDate?.stance === 'NETURAL' ? 1 : (initDate?.speakerNumber ?? 1),
+  );
+
+  const handleSubmit = () => {
+    const totalTime = minutes * 60 + seconds;
+    onSubmit({
+      stance,
+      type: debateType,
+      time: totalTime,
+      ...(speakerNumber !== null && { speakerNumber }),
+    });
+    onClose();
+  };
+
+  const getStanceColor = () => {
+    switch (stance) {
+      case 'PROS':
+        return 'text-blue-500';
+      case 'CONS':
+        return 'text-red-500';
+      case 'NETURAL':
+        return 'text-gray-400';
+      default:
+        return 'text-gray-400';
+    }
+  };
+
+  return (
+    <div className="p-10">
+      <h2 className={`mb-4 text-xl font-bold ${getStanceColor()}`}>
+        타임박스 설정
+      </h2>
+      <div className="flex flex-col space-y-6">
+        <div className="flex items-center space-x-2">
+          <label htmlFor="stance-select" className="w-16 flex-shrink-0">
+            입장
+          </label>
+          <select
+            id="stance-select"
+            className={`flex-1 rounded border p-1 ${
+              stance === 'NETURAL' ? 'cursor-not-allowed bg-gray-200' : ''
+            }`}
+            value={stance}
+            onChange={(e) => setStance(e.target.value as Stance)}
+            disabled={stance === 'NETURAL'}
+          >
+            {stance === 'NETURAL' && <option value="NETURAL"></option>}
+            <option value="PROS">찬성</option>
+            <option value="CONS">반대</option>
+          </select>
+        </div>
+
+        <div className="flex items-center space-x-2">
+          <label htmlFor="debate-type-select" className="w-16 flex-shrink-0">
+            유형
+          </label>
+          <select
+            id="debate-type-select"
+            className="flex-1 rounded border p-1"
+            value={debateType}
+            onChange={(e) => {
+              if (e.target.value === 'TIME_OUT') {
+                setStance('NETURAL');
+              } else {
+                setStance(selectedStance);
+              }
+              setDebateType(e.target.value as DebateType);
+            }}
+          >
+            <option value="OPENING">입론</option>
+            <option value="REBUTTAL">반론</option>
+            <option value="CROSS">교차질의</option>
+            <option value="CLOSING">최종발언</option>
+            <option value="TIME_OUT">작전시간</option>
+          </select>
+        </div>
+
+        <div className="flex w-full items-center space-x-2">
+          <label htmlFor="minutes-input" className="w-16 flex-shrink-0">
+            시간
+          </label>
+          <div className="flex w-full min-w-1 flex-wrap space-x-2">
+            <div className="flex min-w-12 flex-1 items-center">
+              <input
+                id="minutes-input"
+                type="number"
+                min={0}
+                className="min-w-10 flex-grow rounded border p-1 text-center"
+                value={minutes.toString()}
+                onChange={(e) => setMinutes(Number(e.target.value))}
+              />
+              <span className="ml-1 flex-shrink-0">분</span>
+            </div>
+            <div className="flex min-w-12 flex-1 items-center">
+              <input
+                id="seconds-input"
+                type="number"
+                min={0}
+                className="min-w-10 flex-grow rounded border p-1 text-center"
+                value={seconds.toString()}
+                onChange={(e) => setSeconds(Number(e.target.value))}
+              />
+              <span className="ml-1 flex-shrink-0">초</span>
+            </div>
+          </div>
+        </div>
+
+        <div className="flex min-w-0 items-center space-x-2">
+          <label htmlFor="speaker-number-input" className="w-16 flex-shrink-0">
+            발언자
+          </label>
+          <select
+            id="speaker-number-input"
+            className="flex-1 rounded border p-1"
+            value={stance === 'NETURAL' ? 0 : (speakerNumber ?? 0)}
+            onChange={(e) => {
+              setSpeakerNumber(
+                e.target.value === '0' ? null : Number(e.target.value),
+              );
+            }}
+            disabled={stance === 'NETURAL'}
+          >
+            <option value="0">없음</option>
+            <option value="1">1번 토론자</option>
+            <option value="2">2번 토론자</option>
+            <option value="3">3번 토론자</option>
+            <option value="4">4번 토론자</option>
+            <option value="4">5번 토론자</option>
+          </select>
+        </div>
+
+        <button
+          className="mt-4 w-full rounded bg-amber-300 p-2 hover:bg-amber-500"
+          onClick={handleSubmit}
+        >
+          타임박스 설정하기
+        </button>
+      </div>
+    </div>
+  );
+}
