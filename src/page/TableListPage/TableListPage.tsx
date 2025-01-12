54d0ae4d93ddf6420230110475067d9c5154a741
jaeml06 <jaeml0630@gmail.com>
2025-01-12 23:46:54 +0900

    refactor: 더미 데이터 제거

--- src/page/TableListPage/TableListPage.tsx
+++ src/page/TableListPage/TableListPage.tsx
@@ -1,52 +1,14 @@
-import { useState } from 'react';
 import DefaultLayout from '../../layout/defaultLayout/DefaultLayout';
 import { DebateTable } from '../../type/type';
 import AddTable from './components/Table/AddTable';
 import Table from './components/Table/Table';
-
-const initialData: DebateTableWithDelete[] = [
-  {
-    id: 0,
-    name: '테이블 1',
-    type: '의회식 토론',
-    duration: 30,
-    onDelete: () => {},
-  },
-  {
-    id: 1,
-    name: '테이블 2',
-    type: '의회식 토론',
-    duration: 30,
-    onDelete: () => {},
-  },
-  {
-    id: 2,
-    name: '테이블 3',
-    type: '의회식 토론',
-    duration: 30,
-    onDelete: () => {},
-  },
-  {
-    id: 3,
-    name: '테이블 4',
-    type: '의회식 토론',
-    duration: 30,
-    onDelete: () => {},
-  },
-];
-
-interface DebateTableWithDelete extends DebateTable {
-  onDelete: (name: string) => void;
-}
+import { useGetDebateTableList } from '../../hooks/query/useGetDebateTableList';
+import { useDeleteParliamentaryDebateTable } from '../../hooks/mutations/useDeleteParliamentaryDebateTable';
 
 export default function TableListPage() {
-  const [tables, setTables] = useState<DebateTableWithDelete[]>(initialData);
+  const { data: tables } = useGetDebateTableList(1);
 
-  const handleDelete = (targetName: string) => {
-    setTables((prevTables) =>
-      prevTables.filter((table) => table.name !== targetName),
-    );
-  };
+  const { mutate } = useDeleteParliamentaryDebateTable();
 
   return (
     <DefaultLayout>
@@ -55,16 +17,17 @@ export default function TableListPage() {
       </DefaultLayout.Header>
       <div className="flex h-screen flex-col px-4 py-6">
         <main className="grid grid-cols-3 justify-items-center gap-6">
-          {tables.map((table: DebateTableWithDelete, idx: number) => (
-            <Table
-              key={idx}
-              id={table.id}
-              name={table.name}
-              type={table.type}
-              duration={table.duration}
-              onDelete={handleDelete}
-            />
-          ))}
+          {tables &&
+            tables.tables.map((table: DebateTable, idx: number) => (
+              <Table
+                key={idx}
+                id={table.id}
+                name={table.name}
+                type={table.type}
+                duration={table.duration}
+                onDelete={() => mutate({ tableId: table.id, memberId: 1 })}
+              />
+            ))}
           <AddTable />
         </main>
       </div>
