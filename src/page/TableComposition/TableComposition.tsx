3721a4cc0b3b99232a14a3ec548330e1bbf6cc1a
jaeml06 <jaeml0630@gmail.com>
2025-01-12 23:17:54 +0900

    feat: 테이블 생성과 수정을 위한 멀티 스텝 폼, TableComposition 구현

---
+++ src/page/TableComposition/TableComposition.tsx
@@ -0,0 +1,83 @@
+import DefaultLayout from '../../layout/defaultLayout/DefaultLayout';
+import TableNameAndType from './components/TableNameAndType/TableNameAndType';
+import useFunnel from '../../hooks/useFunnel';
+import useTableFrom from './hook/useTableFrom';
+import TimeBoxStep from './components/TimeBoxStep/TimeBoxStep';
+import { useGetParliamentaryTableData } from '../../hooks/query/useGetParliamentaryTableData';
+import { useSearchParams } from 'react-router-dom';
+import { useMemo } from 'react';
+
+export type TableCompositionStep = '이름타입입력' | '타임박스입력';
+
+export default function TableComposition() {
+  const { Funnel, currentStep, goNextStep } =
+    useFunnel<TableCompositionStep>('이름타입입력');
+
+  // 1) URL 등으로부터 "editMode"와 "tableId"를 추출
+  const [searchParams] = useSearchParams();
+  const mode = searchParams.get('mode');
+  const tableId = Number(searchParams.get('tableId') || 0);
+
+  // (2) edit 모드일 때만 서버에서 initData를 가져옴
+  const { data: fetchedTableData } = useGetParliamentaryTableData(
+    tableId,
+    1,
+    mode === 'edit',
+  );
+  const initData = useMemo(() => {
+    if (mode === 'edit' && fetchedTableData) {
+      return {
+        info: {
+          name: fetchedTableData.info.name,
+          agenda: fetchedTableData.info.agenda,
+        },
+        table: fetchedTableData.table,
+      };
+    }
+    return undefined;
+  }, [mode, fetchedTableData]);
+  const { formData, updateInfo, updateTable, AddTable, EditTable } =
+    useTableFrom(currentStep, initData);
+
+  const handleButtonClick = () => {
+    if (mode) {
+      EditTable({
+        memberId: 1,
+        tableId: tableId, // etc
+        tableName: formData.info.name,
+        tableAgenda: formData.info.agenda,
+        table: formData.table,
+      });
+    } else {
+      AddTable({
+        id: 1,
+        tableName: formData.info.name,
+        tableAgenda: formData.info.agenda,
+        table: formData.table,
+      });
+    }
+  };
+  console.log(formData);
+  return (
+    <DefaultLayout>
+      <Funnel
+        step={{
+          이름타입입력: (
+            <TableNameAndType
+              info={formData.info}
+              onNameAndTypeChange={updateInfo}
+              onButtonClick={() => goNextStep('타임박스입력')}
+            />
+          ),
+          타임박스입력: (
+            <TimeBoxStep
+              initTimeBox={formData.table}
+              onTimeBoxChange={updateTable}
+              onButtonClick={handleButtonClick}
+            />
+          ),
+        }}
+      />
+    </DefaultLayout>
+  );
+}
