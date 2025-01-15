ab36ed3e71aa79bf9e7d67777085f7966475e1ed
Shawn Kang <csm06063@naver.com>
2025-01-15 20:36:18 +0900

    refactor: Hoisted all data and functions to the root page

--- src/page/TimerPage/TimerPage.tsx
+++ src/page/TimerPage/TimerPage.tsx
@@ -1,12 +1,16 @@
-import { useState } from 'react';
+import { useState, useRef, useEffect, useCallback } from 'react';
 import DefaultLayout from '../../layout/defaultLayout/DefaultLayout';
 import TimerComponent from './components/TimerComponent';
 import { useQuery } from '@tanstack/react-query';
 import { getParliamentaryTableData, queryKeyIdentifier } from '../../apis/apis';
-// import Spinner from '../common/Spinner';
 import { RiErrorWarningLine } from 'react-icons/ri';
+import DebateInfoSummary from './components/DebateInfoSummary';
 
 export default function TimerPage() {
+  // Load sounds
+  const dingOnceRef = useRef<HTMLAudioElement>(null);
+  const dingTwiceRef = useRef<HTMLAudioElement>(null);
+
   // Prepare data before requesting query
   const tableId = 1024;
   const memberId = 1024;
@@ -24,35 +28,139 @@ export default function TimerPage() {
 
   // Declare states
   const [index, setIndex] = useState<number>(0);
+  const [timer, setTimer] = useState<number>(data ? data.table[index].time : 1);
+  const intervalRef = useRef<NodeJS.Timeout | null>(null);
   const [bg, setBg] = useState<string>('');
-  const updateBg = (bg: string) => {
-    setBg(bg);
-  };
 
-  // Declare functions that manages array's index
-  const increaseIndex = (max: number) => {
-    if (index >= max - 1) {
-      return;
+  // Function that changes background
+  const updateBg = useCallback((bg: string) => {
+    setBg(bg);
+  }, []);
+
+  // Declare functions to handle timer
+  const startTimer = useCallback(() => {
+    console.log('# start timer');
+
+    if (intervalRef.current === null) {
+      intervalRef.current = setInterval(() => {
+        setTimer((prev) => prev - 1);
+      }, 1000);
+      updateBg(
+        timer > 30
+          ? 'gradient-timer-running'
+          : timer < 0
+            ? 'gradient-timer-timeout'
+            : 'gradient-timer-warning',
+      );
     }
-    setIndex(index + 1);
-    //console.log(`# index = ${index}, max = ${max - 1}, data = ` + data!.table[index].time);
-  };
-  const decreaseIndex = () => {
-    if (index <= 0) {
-      return;
+  }, [timer, updateBg]);
+
+  const pauseTimer = useCallback(() => {
+    console.log('# pause timer');
+
+    if (intervalRef.current !== null) {
+      clearInterval(intervalRef.current);
+      intervalRef.current = null;
+      updateBg('');
     }
-    setIndex(index - 1);
-    //console.log(`# index = ${index}, data = ` + data!.table[index]);
-  };
+  }, [updateBg]);
+
+  const resetTimer = useCallback(() => {
+    console.log('# reset timer');
+
+    if (data) setTimer(data.table[index].time);
+    if (intervalRef.current !== null) {
+      clearInterval(intervalRef.current);
+      intervalRef.current = null;
+      updateBg('');
+    }
+  }, [data, index, updateBg]);
+
+  // Declare function to manage parent component's index
+  const moveToOtherItem = useCallback(
+    (goToPrev: boolean) => {
+      resetTimer();
+
+      if (goToPrev) {
+        if (index > 0) {
+          setIndex((prev) => prev - 1);
+        }
+      } else {
+        if (data && index < data.table.length - 1) {
+          setIndex((prev) => prev + 1);
+        }
+      }
+    },
+    [resetTimer, data, index],
+  );
+
+  // Set parent component's background animation by timer's state and remaining time
+  useEffect(() => {
+    if (intervalRef.current === null) {
+      updateBg('');
+    } else if (timer > 30) {
+      updateBg('gradient-timer-running');
+    } else if (timer > 0) {
+      updateBg('gradient-timer-warning');
+    } else {
+      updateBg('gradient-timer-timeout');
+    }
+  }, [timer, updateBg]);
+
+  // Add keyboard event listener
+  useEffect(() => {
+    const handleKeyDown = (event: KeyboardEvent) => {
+      switch (event.code) {
+        case 'Space':
+          if (intervalRef.current !== null) {
+            console.log('# timer paused');
+            pauseTimer();
+          } else {
+            console.log('# timer started');
+            startTimer();
+          }
+          break;
+        case 'ArrowLeft':
+          moveToOtherItem(true);
+          break;
+        case 'ArrowRight':
+          moveToOtherItem(false);
+          break;
+      }
+    };
+
+    // Set listener when component is rendered
+    window.addEventListener('keydown', handleKeyDown);
+    return () => {
+      // Remove listener when component is rendered
+      window.removeEventListener('keydown', handleKeyDown);
+    };
+  }, [pauseTimer, startTimer, moveToOtherItem]);
+
+  // Let timer play sounds when only 30 seconds left or timeout
+  useEffect(() => {
+    if (dingOnceRef.current && timer === 30) {
+      dingOnceRef.current.play();
+    } else if (dingTwiceRef.current && timer === 0) {
+      dingTwiceRef.current.play();
+    }
+  }, [timer]);
+
+  // Let timer initialize itself when data is loaded via api
+  useEffect(() => {
+    if (data) setTimer(data.table[index].time);
+  }, [data, index]);
 
   // Handle exceptions
   if (isLoading) {
     return (
       <div className="flex h-full w-full flex-col items-center justify-center space-y-5">
+        <img src="/spinner.gif" className="size-[120px]" alt="불러오는 중" />
         <h1 className="text-xl font-bold">데이터를 불러오고 있습니다...</h1>
       </div>
     );
   }
+
   if (isError || data === null) {
     return (
       <div className="flex h-full w-full flex-col items-center justify-center space-y-5">
@@ -65,6 +173,7 @@ export default function TimerPage() {
   }
 
   // console.log(`# index = ${index}, data = ` + data!.table[index].time);
+  console.log(`# isRunning = ${intervalRef.current === null}`);
 
   // Return React component
   return (
@@ -92,15 +201,55 @@ export default function TimerPage() {
       </DefaultLayout.Header>
 
       <DefaultLayout.ContentContanier>
+        <audio ref={dingOnceRef} src="/sounds/ding-once-edit.mp3" />
+        <audio ref={dingTwiceRef} src="/sounds/ding-twice-edit.mp3" />
+
         <div className="relative z-10 h-full">
-          <TimerComponent
-            debateInfoList={data!.table}
-            index={index}
-            increaseIndex={() => increaseIndex(data!.table.length)}
-            decreaseIndex={() => decreaseIndex()}
-            updateBg={(bg: string) => updateBg(bg)}
-          />
+          <div className="flex h-full flex-row items-center space-x-4">
+            <div className="flex-1">
+              {index !== 0 && (
+                <DebateInfoSummary
+                  isPrev={true}
+                  moveToOtherItem={(isPrev: boolean) => {
+                    moveToOtherItem(isPrev);
+                  }}
+                  debateInfo={data!.table[index - 1]}
+                />
+              )}
+              {index === 0 && <div className="m-8 w-[240px]"></div>}
+            </div>
+
+            <TimerComponent
+              debateInfo={data!.table[index]}
+              timer={timer}
+              startTimer={() => {
+                startTimer();
+              }}
+              pauseTimer={() => {
+                pauseTimer();
+              }}
+              resetTimer={() => {
+                resetTimer();
+              }}
+            />
+
+            <div className="flex-1">
+              {index !== data!.table.length - 1 && (
+                <DebateInfoSummary
+                  isPrev={false}
+                  moveToOtherItem={(isPrev: boolean) => {
+                    moveToOtherItem(isPrev);
+                  }}
+                  debateInfo={data!.table[index + 1]}
+                />
+              )}
+              {index === data!.table.length - 1 && (
+                <div className="m-8 w-[240px]"></div>
+              )}
+            </div>
+          </div>
         </div>
+
         <div
           className={`absolute inset-0 top-[80px] z-0 animate-gradient opacity-80 ${bg}`}
         ></div>
--- src/page/TimerPage/components/TimerComponent.tsx
+++ src/page/TimerPage/components/TimerComponent.tsx
@@ -1,4 +1,3 @@
-import { useEffect, useRef, useState } from 'react';
 import {
   DebateInfo,
   DebateTypeToString,
@@ -7,176 +6,52 @@ import {
 import Timer from './Timer/Timer';
 import TimerController from './Timer/TimerController';
 import { IoPerson } from 'react-icons/io5';
-import DebateInfoSummary from './DebateInfoSummary';
 
 interface TimerComponentProps {
-  className?: string;
-  debateInfoList: DebateInfo[];
-  index: number;
-  increaseIndex: () => void;
-  decreaseIndex: () => void;
-  updateBg: (newValue: string) => void;
+  debateInfo: DebateInfo;
+  timer: number;
+  startTimer: () => void;
+  pauseTimer: () => void;
+  resetTimer: () => void;
 }
-
-// Define timer's state:
-// - PAUSED: When timer is paused
-// - STOPPED: When timer is rendered or reset
-// - RUNNING: When timer is running
-export type TimerState = 'PAUSED' | 'STOPPED' | 'RUNNING';
 
 // Main timer component that user can control
 export default function TimerComponent({
-  className,
-  debateInfoList,
-  index,
-  increaseIndex,
-  decreaseIndex,
-  updateBg,
+  debateInfo,
+  timer,
+  startTimer,
+  pauseTimer,
+  resetTimer,
 }: TimerComponentProps) {
-  // Load sounds
-  const dingOnceRef = useRef<HTMLAudioElement>(null);
-  const dingTwiceRef = useRef<HTMLAudioElement>(null);
-
-  // Declare states
-  const [timerState, setTimerState] = useState<TimerState>('STOPPED');
-  const [timer, setTimer] = useState<number>(debateInfoList[index].time);
-  const intervalRef = useRef<number | null>(null);
-
   // Set texts to be displayed
   const titleText =
-    debateInfoList[index].type !== 'TIME_OUT'
-      ? `${StanceToString[debateInfoList[index].stance]} ${DebateTypeToString[debateInfoList[index].type]}`
-      : DebateTypeToString[debateInfoList[index].type];
+    debateInfo.type !== 'TIME_OUT'
+      ? `${StanceToString[debateInfo.stance]} ${DebateTypeToString[debateInfo.type]}`
+      : DebateTypeToString[debateInfo.type];
   const speakerText =
-    debateInfoList[index].stance === 'NEUTRAL'
+    debateInfo.stance === 'NEUTRAL'
       ? ''
-      : `${debateInfoList[index].speakerNumber}번 발언자`;
+      : `${debateInfo.speakerNumber}번 발언자`;
 
   // Set background color by debateInfo's stance
   const bgColor =
-    debateInfoList[index].stance === 'NEUTRAL'
+    debateInfo.stance === 'NEUTRAL'
       ? 'bg-zinc-500'
-      : debateInfoList[index].stance === 'PROS'
+      : debateInfo.stance === 'PROS'
         ? 'bg-blue-500'
         : 'bg-red-500';
-
-  // Declare functions to handle timer
-  const startTimer = () => {
-    if (intervalRef.current) return;
-
-    setTimerState('RUNNING');
-    intervalRef.current = window.setInterval(() => {
-      setTimer((prev) => prev - 1);
-    }, 1000);
-  };
-  const pauseTimer = () => {
-    setTimerState('PAUSED');
-    if (intervalRef.current) {
-      clearInterval(intervalRef.current);
-      intervalRef.current = null;
-    }
-  };
-  const resetTimer = () => {
-    setTimerState('STOPPED');
-    if (intervalRef.current) {
-      clearInterval(intervalRef.current);
-      intervalRef.current = null;
-    }
-  };
-
-  // Declare function to manage parant component's index
-  const moveToOtherItem = (isPrev: boolean) => {
-    if (isPrev) {
-      decreaseIndex();
-    } else {
-      increaseIndex();
-    }
-    resetTimer();
-  };
-
-  // Set parant component's background animation by timer's state and remaining time
-  useEffect(() => {
-    if (timerState !== 'RUNNING') {
-      updateBg('');
-    } else {
-      if (timer > 30) {
-        updateBg('gradient-timer-running');
-      } else if (timer <= 30 && timer > 0) {
-        updateBg('gradient-timer-warning');
-      } else {
-        updateBg('gradient-timer-timeout');
-      }
-    }
-  }, [timerState, timer, updateBg]);
-
-  // Add keyboard event listener
-  useEffect(() => {
-    const handleKeyDown = (event: KeyboardEvent) => {
-      switch (event.code) {
-        case 'Space':
-          if (timerState === 'RUNNING') {
-            pauseTimer();
-          } else {
-            startTimer();
-          }
-          break;
-        case 'ArrowLeft':
-          moveToOtherItem(true);
-          break;
-        case 'ArrowRight':
-          moveToOtherItem(false);
-          break;
-      }
-    };
-
-    window.addEventListener('keydown', handleKeyDown);
-    return () => {
-      window.removeEventListener('keydown', handleKeyDown);
-    };
-  });
-
-  // Let timer play sounds when only 30 seconds left or timeout
-  useEffect(() => {
-    if (dingOnceRef.current && timer === 30) {
-      dingOnceRef.current.play();
-    } else if (dingTwiceRef.current && timer === 0) {
-      dingTwiceRef.current.play();
-    }
-  });
-
-  // Let timer be initiated with default time that is defined in current debateInfo's time when index is changed
-  useEffect(() => {
-    setTimer(debateInfoList[index].time);
-    // console.log(`# Timer has set to index ${index} and ${debateInfoList[index].time}`);
-  }, [debateInfoList, setTimer, index]);
 
   // Return React component
   return (
     <div className="flex h-full flex-row items-center space-x-4">
-      <audio ref={dingOnceRef} src="/sounds/ding-once-edit.mp3" />
-      <audio ref={dingTwiceRef} src="/sounds/ding-twice-edit.mp3" />
-
-      <div className="flex-1">
-        {index !== 0 && (
-          <DebateInfoSummary
-            isPrev={true}
-            moveToOtherItem={(isPrev: boolean) => {
-              moveToOtherItem(isPrev);
-            }}
-            debateInfo={debateInfoList[index - 1]}
-          />
-        )}
-        {index === 0 && <div className="m-8 w-[240px]"></div>}
-      </div>
-
       <div
-        className={`flex w-min flex-col items-center rounded-[50px] border-4 border-zinc-50 px-8 py-8 shadow-2xl ${bgColor} ${className !== undefined ? className : ''}`}
+        className={`flex w-min flex-col ${bgColor} items-center rounded-[50px] border-4 border-zinc-50 px-8 py-8 shadow-2xl`}
       >
         {/* Title */}
         <div className="m-2 mb-8 flex flex-col items-center space-y-3">
           <h1 className="text-6xl font-bold text-zinc-50">{titleText}</h1>
           <div className="flex flex-row items-center space-x-3 text-zinc-50">
-            {debateInfoList[index].stance !== 'NEUTRAL' && (
+            {debateInfo.stance !== 'NEUTRAL' && (
               <IoPerson className="size-[25px]" />
             )}
             <h1 className="text-3xl">{speakerText}</h1>
@@ -192,21 +67,6 @@ export default function TimerComponent({
           onStart={startTimer}
           onPause={pauseTimer}
         />
-      </div>
-
-      <div className="flex-1">
-        {index !== debateInfoList.length - 1 && (
-          <DebateInfoSummary
-            isPrev={false}
-            moveToOtherItem={(isPrev: boolean) => {
-              moveToOtherItem(isPrev);
-            }}
-            debateInfo={debateInfoList[index + 1]}
-          />
-        )}
-        {index === debateInfoList.length - 1 && (
-          <div className="m-8 w-[240px]"></div>
-        )}
       </div>
     </div>
   );
