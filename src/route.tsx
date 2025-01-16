41edad5c1f6d6a67ba93985aa9855b83f710361e
Shawn Kang <csm06063@naver.com>
2025-01-16 20:34:15 +0900

    feat: Wrapped router with ErrorBoundaryWrapper

--- src/route.tsx
+++ src/route.tsx
@@ -4,32 +4,33 @@ import TableListPage from './page/TableListPage/TableListPage';
 import TableOverview from './page/TableOverviewPage/TableOverview';
 import TimerPage from './page/TimerPage/TimerPage';
 import TableSetup from './page/TableSetupPage/TableSetup';
-import ErrorBoundary from './components/ErrorBoundary/ErrorBoundary';
+import ErrorBoundaryWrapper from './components/ErrorBoundary/ErrorBoundaryWrapper';
 
 const router = createBrowserRouter([
   {
-    path: '/',
-    element: <TableSetup />,
-  },
-  {
-    path: '/login',
-    element: <LoginPage />,
-  },
-  {
-    path: '/table',
-    element: <TableListPage />,
-  },
-  {
-    path: '/overview/:id',
-    element: <TableOverview />,
-  },
-  {
-    path: '/table/parliamentary/:id',
-    element: (
-      <ErrorBoundary>
-        <TimerPage />
-      </ErrorBoundary>
-    ),
+    element: <ErrorBoundaryWrapper />,
+    children: [
+      {
+        path: '/',
+        element: <TableSetup />,
+      },
+      {
+        path: '/login',
+        element: <LoginPage />,
+      },
+      {
+        path: '/table',
+        element: <TableListPage />,
+      },
+      {
+        path: '/overview/:id',
+        element: <TableOverview />,
+      },
+      {
+        path: '/table/parliamentary/:id',
+        element: <TimerPage />,
+      },
+    ],
   },
 ]);
 
