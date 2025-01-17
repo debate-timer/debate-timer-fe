547ce55d3f8e290136265694dc481e469b791a45
Shawn Kang <77564014+i-meant-to-be@users.noreply.github.com>
2025-01-17 17:07:45 +0900

    [FEAT] `ErrorBoundary` 도입 (#65)
    * feat: ErrorBoundary 도입
    
    * feat: Wrapped router with ErrorBoundaryWrapper
    
    * feat: Enabled 'throwOnError' option on QueryClient
    
    * feat: Added refresh button on ErrorPage
    
    * refactor: Applied feedbacks from PR review
    
    - Declared string constants for ErrorBoundary
    - Set icon size on 'size' parameter instead of TailwindCSS 'className'

--- src/route.tsx
+++ src/route.tsx
@@ -4,27 +4,33 @@ import TableListPage from './page/TableListPage/TableListPage';
 import TableOverview from './page/TableOverviewPage/TableOverview';
 import TimerPage from './page/TimerPage/TimerPage';
 import TableSetup from './page/TableSetupPage/TableSetup';
+import ErrorBoundaryWrapper from './components/ErrorBoundary/ErrorBoundaryWrapper';
 
 const router = createBrowserRouter([
   {
-    path: '/',
-    element: <TableSetup />,
-  },
-  {
-    path: '/login',
-    element: <LoginPage />,
-  },
-  {
-    path: '/table',
-    element: <TableListPage />,
-  },
-  {
-    path: '/overview/:id',
-    element: <TableOverview />,
-  },
-  {
-    path: '/table/parliamentary/:id',
-    element: <TimerPage />,
+    element: <ErrorBoundaryWrapper />,
+    children: [
+      {
+        path: '/',
+        element: <TableSetup />,
+      },
+      {
+        path: '/login',
+        element: <LoginPage />,
+      },
+      {
+        path: '/table',
+        element: <TableListPage />,
+      },
+      {
+        path: '/overview/:id',
+        element: <TableOverview />,
+      },
+      {
+        path: '/table/parliamentary/:id',
+        element: <TimerPage />,
+      },
+    ],
   },
 ]);
 
