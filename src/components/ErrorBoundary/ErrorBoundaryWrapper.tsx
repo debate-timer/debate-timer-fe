e157b6052b2311f7071edb8a576b5a8f2d9a6b41
eunwoo-levi <eunwoo1341@gmail.com>
2025-02-07 01:12:04 +0900

    ErrorBoundary에 GoogleAnalytics 컴포넌트 추가

--- src/components/ErrorBoundary/ErrorBoundaryWrapper.tsx
+++ src/components/ErrorBoundary/ErrorBoundaryWrapper.tsx
@@ -1,9 +1,11 @@
 import { Outlet } from 'react-router-dom';
 import ErrorBoundary from './ErrorBoundary';
+import GoogleAnalytics from '../../util/GoogleAnalytics';
 
 export default function ErrorBoundaryWrapper() {
   return (
     <ErrorBoundary>
+      <GoogleAnalytics />
       <Outlet />
     </ErrorBoundary>
   );
