31e19d282235b211a54399bdf8e5b1aad7588b23
jaeml06 <jaeml0630@gmail.com>
2025-02-20 22:53:19 +0900

    feat: HeaderTitle ellipsis 추가

--- src/components/HeaderTableInfo/HeaderTableInfo.tsx
+++ src/components/HeaderTableInfo/HeaderTableInfo.tsx
@@ -1,11 +1,10 @@
 interface HeaderTitleProps {
-  name: string | undefined;
+  name?: string;
 }
 
 export default function HeaderTableInfo(props: HeaderTitleProps) {
   const { name } = props;
-  const displayName =
-    name === undefined || name.trim() === '' ? '테이블 이름 없음' : name;
+  const displayName = !name?.trim() ? '테이블 이름 없음' : name.trim();
   return (
     <div className="flex flex-col space-y-[4px]">
       <h1 className="text-sm">의회식</h1>
--- src/components/HeaderTitle/HeaderTitle.tsx
+++ src/components/HeaderTitle/HeaderTitle.tsx
@@ -1,10 +1,13 @@
 interface HeaderTitleProps {
-  title: string | undefined;
+  title?: string;
 }
 
-export default function HeaderTitle(props: HeaderTitleProps) {
-  const { title } = props;
-  const displayTitle =
-    title === undefined || title.trim() === '' ? '주제 없음' : title;
-  return <h1 className="text-3xl font-bold">{displayTitle}</h1>;
+export default function HeaderTitle({ title }: HeaderTitleProps) {
+  const displayTitle = !title?.trim() ? '주제 없음' : title.trim();
+
+  return (
+    <h1 className="w-full max-w-[50vw] overflow-hidden text-ellipsis whitespace-nowrap text-3xl font-bold">
+      {displayTitle}
+    </h1>
+  );
 }
