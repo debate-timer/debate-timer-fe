aefa0accea4843318ed06f51bb4b7cd2ba513606
Shawn Kang <csm06063@naver.com>
2025-02-26 14:10:32 +0900

    design: Let TableListPage display columns responsibily

--- src/layout/defaultLayout/DefaultLayout.tsx
+++ src/layout/defaultLayout/DefaultLayout.tsx
@@ -12,4 +12,5 @@ function DefaultLayout(props: PropsWithChildren) {
 DefaultLayout.Header = StickyTriSectionHeader;
 DefaultLayout.ContentContanier = ContentContanier;
 DefaultLayout.StickyFooterWrapper = StickyFooterWrapper;
+
 export default DefaultLayout;
