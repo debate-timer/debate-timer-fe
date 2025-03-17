3481178413a8106b12f886a14ec434001ca27bd8
jaeml06 <jaeml0630@gmail.com>
2025-03-17 17:12:05 +0900

    [FIX] 초기 speakerNumber를 상황에 맞게 초기화 (#186)
    * fix: 초기 speakerNumber를 상황에 맞게 초기화
    
    * fix: 토론자 초기값 세분화

--- src/page/TableComposition/components/TimerCreationContent/TimerCreationContent.tsx
+++ src/page/TableComposition/components/TimerCreationContent/TimerCreationContent.tsx
@@ -40,9 +40,15 @@ export default function TimerCreationContent({
   const [minutes, setMinutes] = useState(initMinutes);
   const [seconds, setSeconds] = useState(initSeconds);
   const [speakerNumber, setSpeakerNumber] = useState<number | null>(
-    (beforeData?.speakerNumber ?? initData?.stance === 'NEUTRAL')
-      ? null
-      : (initData?.speakerNumber ?? 1),
+    beforeData?.speakerNumber
+      ? beforeData.speakerNumber
+      : initData
+        ? initData.stance === 'NEUTRAL'
+          ? null
+          : initData.speakerNumber
+            ? initData.speakerNumber
+            : null
+        : 1,
   );
 
   const handleSubmit = () => {
