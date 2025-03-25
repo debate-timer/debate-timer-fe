791492982cbd7fca35cee680f1920ce2d1c17a19
jaeml06 <jaeml0630@gmail.com>
2025-03-25 10:32:39 +0900

    test: 페이지가 잘리는 문제 수정

--- src/page/CustomizeTimerPage/TimerPage.stories.tsx
+++ src/page/CustomizeTimerPage/TimerPage.stories.tsx
@@ -5,10 +5,19 @@ const meta: Meta<typeof FreediscussionTimerPage> = {
   title: 'page/CustomizeTimerPage',
   component: FreediscussionTimerPage,
   tags: ['autodocs'],
+  parameters: {
+    layout: 'fullscreen', // Storybook에서 전체 화면으로 표시
+  },
 };
 
 export default meta;
 
 type Story = StoryObj<typeof FreediscussionTimerPage>;
 
-export const Default: Story = {};
+export const Default: Story = {
+  render: () => (
+    <div style={{ height: '100vh', width: '100vw', overflow: 'hidden' }}>
+      <FreediscussionTimerPage />
+    </div>
+  ),
+};
