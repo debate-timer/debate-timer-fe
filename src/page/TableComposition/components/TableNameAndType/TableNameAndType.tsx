33aa3f91e98fa6d68690fce8d11bef3ea87c9b60
Shawn Kang <77564014+i-meant-to-be@users.noreply.github.com>
2025-03-29 14:20:10 +0900

    [DESIGN] 작전 시간 타이머 추가에 따른 공통 컴포넌트 분리 (#200)
    * refactor: 이전/다음 차례 버튼 공통 컴포넌트로 분리
    
    * refactor: Applied new PrevNextButton on timer
    
    * refactor: 결정 버튼 공통 컴포넌트로 분리
    
    * refactor: Added 'enabled' options on DecisionButton
    
    * refactor: Deleted files related with DecisionButton
    
    * refactor: Added CSS properties for common buttons
    
    * refactor: Updated CSS properties
    
    * design: Applied changes on CSS button properties
    
    * design: Added and applied non-hover CSS property
    
    * design: Gave more bottom margin below buttons
    
    * refactor: Changed component name
    
    * refactor: Applied new component name
    
    * design: Added shadow on RoundControlButton
    
    * design: Neutral 색상의 호버 버튼 변경 사항 적용
    
    * fix: 호버 색상 변경

--- src/page/TableComposition/components/TableNameAndType/TableNameAndType.tsx
+++ src/page/TableComposition/components/TableNameAndType/TableNameAndType.tsx
@@ -108,7 +108,7 @@ export default function TableNameAndType(props: TableNameAndTypeProps) {
       </DefaultLayout.ContentContainer>
 
       <DefaultLayout.StickyFooterWrapper>
-        <div className="mx-auto mb-4 w-full max-w-4xl">
+        <div className="mx-auto mb-8 w-full max-w-4xl">
           <button
             onClick={() => {
               if (info.name === '') {
@@ -119,7 +119,7 @@ export default function TableNameAndType(props: TableNameAndTypeProps) {
               }
               onButtonClick();
             }}
-            className="h-16 w-full rounded-md bg-brand-main text-lg font-semibold transition-colors duration-300 hover:bg-amber-500 md:text-xl"
+            className="button enabled h-16 w-full"
           >
             다음
           </button>
