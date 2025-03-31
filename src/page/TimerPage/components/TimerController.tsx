4732e3da2c8435b80784d1b52e549c840199e255
Shawn Kang <shineksu813@outlook.com>
2025-03-31 15:16:38 +0900

    refactor: TimerController 구조 수정

--- src/page/TimerPage/components/TimerController.tsx
+++ src/page/TimerPage/components/TimerController.tsx
@@ -22,6 +22,55 @@ export default function TimerController({
   return (
     <div
       data-testid="timer-controller"
+      className="flex h-[180px] w-[730px] flex-row"
+    >
+      <div className="flex size-full flex-1 items-center justify-end ">
+        <button
+          className="size-[82px] rounded-full bg-neutral-900 p-[18px] hover:bg-brand-main"
+          onClick={() => onReset()}
+        >
+          <FiRefreshCcw className="size-full justify-center text-slate-50" />
+        </button>
+      </div>
+      <div className="flex size-full flex-1 items-center justify-center ">
+        {' '}
+        <button
+          className="size-[152px] rounded-full bg-neutral-900 p-[45px] hover:bg-brand-main"
+          onClick={() => {
+            if (isRunning) {
+              onPause();
+            } else {
+              onStart();
+            }
+          }}
+        >
+          {isRunning && (
+            <GiPauseButton className="size-full justify-center text-neutral-50" />
+          )}
+          {!isRunning && (
+            <FaPlay className="size-full justify-center text-neutral-50" />
+          )}
+        </button>
+      </div>
+      <div className="flex size-full flex-1 items-center justify-start ">
+        {' '}
+        {isTimerChangeable && (
+          <button
+            data-testid="additional-timer-button"
+            className="h-[133px] w-[165px] flex-col items-center space-y-2 rounded-[23px] border-[3px] border-neutral-900 bg-neutral-50 text-[30px] font-bold leading-[37px] hover:bg-neutral-200"
+            onClick={() => onChangingTimer()}
+          >
+            <p>작전 시간</p>
+            <p>사용</p>
+          </button>
+        )}
+      </div>
+    </div>
+  );
+
+  return (
+    <div
+      data-testid="timer-controller"
       className="relative h-[180px] w-[730px]"
     >
       {/* Left-sided element */}
