513c8d148005e92cfd478b6475f08c506ed978d0
katie424 <7080katie@gmail.com>
2025-04-04 00:11:09 +0900

    design: 타임박스 추가 버튼 중앙 정렬

--- src/page/TableComposition/components/TimerCreationButton/TimerCreationButton.tsx
+++ src/page/TableComposition/components/TimerCreationButton/TimerCreationButton.tsx
@@ -4,9 +4,9 @@ export default function TimerCreationButton(
   props: ButtonHTMLAttributes<HTMLButtonElement>,
 ) {
   return (
-    <div className="m-4 flex w-full items-center justify-center">
+    <div className="my-4 flex w-full items-center justify-center">
       <button
-        className="flex h-10 w-4/5 items-center justify-center rounded-md bg-neutral-300 font-bold hover:bg-neutral-500"
+        className="flex h-10 w-4/5 items-center justify-center rounded-md bg-neutral-300 font-bold hover:bg-neutral-400"
         {...props}
       >
         +
