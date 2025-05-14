f7b2714c67207dc29d0b81c86b234c35e0e7b120
Shawn Kang <csm06063@naver.com>
2025-05-15 01:07:21 +0900

    feat: TableOverviewPage에 공유 버튼 추가

--- src/page/TableOverviewPage/TableOverview.tsx
+++ src/page/TableOverviewPage/TableOverview.tsx
@@ -10,6 +10,8 @@ import usePatchParliamentaryTable from '../../hooks/mutations/usePatchParliament
 import usePatchCustomizeTable from '../../hooks/mutations/usePatchCustomizeDebateTable';
 import { useGetCustomizeTableData } from '../../hooks/query/useGetCustomizeTableData';
 import CustomizeDebatePanel from '../TableComposition/components/DebatePanel/CustomizeDebatePanel';
+import { IoShareOutline } from 'react-icons/io5';
+import { useTableShare } from '../../hooks/useTableShare';
 
 export default function TableOverview() {
   const { type, id } = useParams();
@@ -41,79 +43,97 @@ export default function TableOverview() {
     navigate(`/table/customize/${tableId}`);
   });
 
+  // Hook for sharing tables
+  const { openShareModal, TableShareModal } = useTableShare(tableId);
+
   return (
-    <DefaultLayout>
-      <DefaultLayout.Header>
-        <DefaultLayout.Header.Left>
-          <HeaderTableInfo
-            name={data?.info.name}
-            type={isCustomize ? 'CUSTOMIZE' : 'PARLIAMENTARY'}
-          />
-        </DefaultLayout.Header.Left>
-        <DefaultLayout.Header.Center>
-          <HeaderTitle title={data?.info.agenda} />
-        </DefaultLayout.Header.Center>
-        <DefaultLayout.Header.Right defaultIcons={['home', 'logout']} />
-      </DefaultLayout.Header>
-
-      <DefaultLayout.ContentContainer>
-        <section className="mx-auto flex w-full max-w-4xl flex-col justify-center">
-          {isCustomize && customizeData ? (
-            <PropsAndConsTitle
-              prosTeamName={customizeData.info.prosTeamName}
-              consTeamName={customizeData.info.consTeamName}
+    <>
+      <DefaultLayout>
+        <DefaultLayout.Header>
+          <DefaultLayout.Header.Left>
+            <HeaderTableInfo
+              name={data?.info.name}
+              type={isCustomize ? 'CUSTOMIZE' : 'PARLIAMENTARY'}
             />
-          ) : (
-            <PropsAndConsTitle />
-          )}
+          </DefaultLayout.Header.Left>
+          <DefaultLayout.Header.Center>
+            <HeaderTitle title={data?.info.agenda} />
+          </DefaultLayout.Header.Center>
+          <DefaultLayout.Header.Right defaultIcons={['home', 'logout']} />
+        </DefaultLayout.Header>
 
-          <div className="flex w-full flex-col gap-2">
-            {isCustomize && customizeData
-              ? customizeData.table.map((info, index) => (
-                  <CustomizeDebatePanel
-                    key={index}
-                    info={info}
-                    prosTeamName={customizeData.info.prosTeamName}
-                    consTeamName={customizeData.info.consTeamName}
-                  />
-                ))
-              : parliamentaryData?.table.map((info, index) => (
-                  <DebatePanel key={index} info={info} />
-                ))}
+        <DefaultLayout.ContentContainer>
+          <section className="mx-auto flex w-full max-w-4xl flex-col justify-center">
+            {isCustomize && customizeData ? (
+              <PropsAndConsTitle
+                prosTeamName={customizeData.info.prosTeamName}
+                consTeamName={customizeData.info.consTeamName}
+              />
+            ) : (
+              <PropsAndConsTitle />
+            )}
+
+            <div className="flex w-full flex-col gap-2">
+              {isCustomize && customizeData
+                ? customizeData.table.map((info, index) => (
+                    <CustomizeDebatePanel
+                      key={index}
+                      info={info}
+                      prosTeamName={customizeData.info.prosTeamName}
+                      consTeamName={customizeData.info.consTeamName}
+                    />
+                  ))
+                : parliamentaryData?.table.map((info, index) => (
+                    <DebatePanel key={index} info={info} />
+                  ))}
+            </div>
+          </section>
+        </DefaultLayout.ContentContainer>
+
+        <DefaultLayout.StickyFooterWrapper>
+          <div className="mx-auto mb-8 flex w-full max-w-4xl items-center justify-between gap-2">
+            <button
+              className="button enabled-hover-neutral h-16 w-full"
+              onClick={() =>
+                navigate(
+                  `/composition?mode=edit&tableId=${tableId}&type=${type?.toUpperCase()}`,
+                )
+              }
+            >
+              <div className="flex items-center justify-center gap-2">
+                <RiEditFill />
+                수정하기
+              </div>
+            </button>
+            <div className="flex h-16 w-full space-x-2">
+              <button
+                className="button enabled flex-1"
+                onClick={() =>
+                  isCustomize
+                    ? patchCustomizeTableMutation.mutate({ tableId })
+                    : patchParliamentaryTableMutation.mutate({ tableId })
+                }
+              >
+                <div className="flex items-center justify-center gap-2">
+                  <RiSpeakFill />
+                  토론하기
+                </div>
+              </button>
+
+              <button
+                className="button enabled flex size-16 items-center justify-center"
+                onClick={() => {
+                  openShareModal();
+                }}
+              >
+                <IoShareOutline />
+              </button>
+            </div>
           </div>
-        </section>
-      </DefaultLayout.ContentContainer>
+        </DefaultLayout.StickyFooterWrapper>
+      </DefaultLayout>
 
-      <DefaultLayout.StickyFooterWrapper>
-        <div className="mx-auto mb-8 flex w-full max-w-4xl items-center justify-between gap-2">
-          <button
-            className="button enabled-hover-neutral h-16 w-full"
-            onClick={() =>
-              navigate(
-                `/composition?mode=edit&tableId=${tableId}&type=${type?.toUpperCase()}`,
-              )
-            }
-          >
-            <div className="flex items-center justify-center gap-2">
-              <RiEditFill />
-              수정하기
-            </div>
-          </button>
-          <button
-            className="button enabled h-16 w-full"
-            onClick={() =>
-              isCustomize
-                ? patchCustomizeTableMutation.mutate({ tableId })
-                : patchParliamentaryTableMutation.mutate({ tableId })
-            }
-          >
-            <div className="flex items-center justify-center gap-2">
-              <RiSpeakFill />
-              토론하기
-            </div>
-          </button>
-        </div>
-      </DefaultLayout.StickyFooterWrapper>
-    </DefaultLayout>
+      <TableShareModal />
+    </>
   );
 }
