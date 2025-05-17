a7e820becc36f1fdac896eae81308e24423b7248
katie424 <7080katie@gmail.com>
2025-05-17 16:35:10 +0900

    test: setup storybook viewport

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -7,6 +7,7 @@ import { MemoryRouter, Routes, Route } from 'react-router-dom';
 import React from 'react';
 import { initialize, mswLoader } from 'msw-storybook-addon';
 import { allHandlers } from '../src/mocks/handlers/global';
+import { INITIAL_VIEWPORTS } from '@storybook/addon-viewport';
 
 initialize();
 
@@ -22,6 +23,28 @@ const preview: Preview = {
         color: /(background|color)$/i,
         date: /Date$/i,
       },
+    },
+    viewport: {
+      viewports: {
+        ...INITIAL_VIEWPORTS,
+        laptop1024: {
+          name: 'Laptop 1024',
+          styles: {
+            width: '1024px',
+            height: '768px',
+          },
+          type: 'desktop',
+        },
+        desktop1440: {
+          name: 'Desktop 1440',
+          styles: {
+            width: '1440px',
+            height: '900px',
+          },
+          type: 'desktop',
+        },
+      },
+      defaultViewport: 'responsive',
     },
   },
   loaders: [mswLoader],
