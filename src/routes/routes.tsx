e9c41491855e0e141cbd9c2db1b868b5abc04333
Shawn Kang <csm06063@naver.com>
2025-05-21 23:32:28 +0900

    chore: Applied timer name change on routes.tsx

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -6,10 +6,10 @@ import TableComposition from '../page/TableComposition/TableComposition';
 import ErrorBoundaryWrapper from '../components/ErrorBoundary/ErrorBoundaryWrapper';
 import ProtectedRoute from './ProtectedRoute';
 import OAuth from '../page/LoginPage/OAuth';
-import CustomizeTimerPage from '../page/CustomizeTimerPage/CustomizeTimerPage';
 import ReactGA from 'react-ga4';
 import NotFoundPage from '../components/ErrorBoundary/NotFoundPage';
 import BackActionHandler from '../components/BackActionHandler';
+import TimerPage from '../page/TimerPage/TimerPage';
 
 const routesConfig = [
   {
@@ -34,7 +34,7 @@ const routesConfig = [
   },
   {
     path: '/table/customize/:id',
-    element: <CustomizeTimerPage />,
+    element: <TimerPage />,
     requiresAuth: true,
   },
   {
