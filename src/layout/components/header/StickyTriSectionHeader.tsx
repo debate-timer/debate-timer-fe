016d4220e74234186f56ab5f8917635c100cfe88
jaeml06 <jaeml0630@gmail.com>
2025-05-31 23:04:14 +0900

    feat: 헤더에 로그인, 로그아웃 분기 버튼 구현

--- src/layout/components/header/StickyTriSectionHeader.tsx
+++ src/layout/components/header/StickyTriSectionHeader.tsx
@@ -4,8 +4,13 @@ import { useNavigate } from 'react-router-dom';
 import useLogout from '../../../hooks/mutations/useLogout';
 import { IoLogIn, IoLogOut } from 'react-icons/io5';
 import IconButton from '../../../components/IconButton/IconButton';
-import { isGuestFlow } from '../../../util/sessionStorage';
+import {
+  deleteSessionCustomizeTableData,
+  isGuestFlow,
+} from '../../../util/sessionStorage';
 import { AuthLogin } from '../../../util/googleAuth';
+import { useModal } from '../../../hooks/useModal';
+import LoginAndStoreDBModal from '../../../components/LoginAndStoreDBModal/LoginAndStoreDBModal';
 
 function StickyTriSectionHeader(props: PropsWithChildren) {
   const { children } = props;
@@ -39,53 +44,74 @@ StickyTriSectionHeader.Right = function Right({
 }: RightProps) {
   const navigate = useNavigate();
   const { mutate: logoutMutate } = useLogout(() => navigate('/login'));
+  const { openModal, closeModal, ModalWrapper } = useModal({});
   return (
-    <div className="flex flex-1  items-stretch justify-end gap-2 text-right">
-      {children && (
-        <>
-          {children}
-          <div className="w-[1px] self-stretch bg-neutral-500" />
-        </>
-      )}
+    <>
+      <div className="flex flex-1  items-stretch justify-end gap-2 text-right">
+        {children && (
+          <>
+            {children}
+            <div className="w-[1px] self-stretch bg-neutral-500" />
+          </>
+        )}
 
-      {defaultIcons?.map((iconName, index) => {
-        switch (iconName) {
-          case 'home':
-            return (
-              <div key={`${iconName}-${index}`}>
-                <IconButton
-                  icon={<IoMdHome size={24} />}
-                  onClick={() => navigate('/')}
-                />
-              </div>
-            );
-          case 'auth':
-            if (isGuestFlow()) {
+        {defaultIcons?.map((iconName, index) => {
+          switch (iconName) {
+            case 'home':
               return (
                 <div key={`${iconName}-${index}`}>
                   <IconButton
-                    icon={<IoLogIn size={24} />}
-                    onClick={AuthLogin}
-                    title="로그인 하기"
+                    icon={<IoMdHome size={24} />}
+                    onClick={() => navigate('/')}
                   />
                 </div>
               );
-            } else {
-              return (
-                <div key={`${iconName}-${index}`}>
-                  <IconButton
-                    icon={<IoLogOut size={24} />}
-                    onClick={() => logoutMutate()}
-                    title="로그아웃 하기"
-                  />
-                </div>
-              );
-            }
-          default:
-            return null;
-        }
-      })}
-    </div>
+            case 'auth':
+              if (isGuestFlow()) {
+                return (
+                  <div key={`${iconName}-${index}`}>
+                    <IconButton
+                      icon={<IoLogIn size={24} />}
+                      onClick={() => openModal()}
+                      title="로그인 하기"
+                    />
+                  </div>
+                );
+              } else {
+                return (
+                  <div key={`${iconName}-${index}`}>
+                    <IconButton
+                      icon={<IoLogOut size={24} />}
+                      onClick={() => logoutMutate()}
+                      title="로그아웃 하기"
+                    />
+                  </div>
+                );
+              }
+            default:
+              return null;
+          }
+        })}
+      </div>
+      <ModalWrapper closeButtonColor="text-neutral-1000">
+        <LoginAndStoreDBModal
+          onConfirm={() => {
+            closeModal();
+            AuthLogin();
+          }}
+          onDecline={() => {
+            deleteSessionCustomizeTableData();
+            closeModal();
+            AuthLogin();
+          }}
+        >
+          <>
+            작성하신 내용이 저장되지 않을 수 있습니다. <br />
+            그래도 로그인을 진행하시겠습니까?
+          </>
+        </LoginAndStoreDBModal>
+      </ModalWrapper>
+    </>
   );
 };
 
