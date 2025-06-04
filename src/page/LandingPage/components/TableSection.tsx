f35c1f0f788033376011ad5afdf82ed5cb6f0335
katie424 <7080katie@gmail.com>
2025-06-04 22:44:02 +0900

    refactor: h1 태그 중복 제거

--- src/page/LandingPage/components/TableSection.tsx
+++ src/page/LandingPage/components/TableSection.tsx
@@ -13,9 +13,9 @@ export default function TableSection({ onLogin }: TableSectionProps) {
           <span className="relative z-10">홈 | 설정</span>
           <span className="absolute bottom-0 left-0 z-0 h-4 w-full bg-brand-main/70"></span>
         </div>
-        <h1 className="mt-4 text-left text-[2.75vw] font-bold">
+        <h3 className="mt-4 text-left text-[2.75vw] font-bold">
           토론 정보 관리 및 기록
-        </h1>
+        </h3>
       </div>
       <div className="flex flex-row items-center justify-center gap-8 px-4">
         <img src={section301} alt="section301" className="flex w-1/2" />
@@ -28,7 +28,7 @@ export default function TableSection({ onLogin }: TableSectionProps) {
       </div>
       <div className="flex flex-row items-center justify-center gap-8 px-4">
         <div className="flex w-1/2 flex-col items-center gap-4">
-          <h1 className="text-[1.75vw] font-semibold">시간표 목록</h1>
+          <h3 className="text-[1.75vw] font-semibold">시간표 목록</h3>
           <p className="text-[1.5vw] font-medium text-neutral-600">
             내가 만든 시간표를 저장하고 싶나요?
           </p>
@@ -37,8 +37,8 @@ export default function TableSection({ onLogin }: TableSectionProps) {
       </div>
 
       <div className="flex flex-col items-center justify-center gap-1 text-[2.75vw] font-bold">
-        <h1>시간표를 저장하려면,</h1>
-        <h1>디베이트 타이머에 로그인해보세요!</h1>
+        <p>시간표를 저장하려면,</p>
+        <p>디베이트 타이머에 로그인해보세요!</p>
         <button
           className="mt-14 rounded-full border border-neutral-300 bg-brand-main px-5 py-2 text-[1.25vw] font-medium text-black transition-all duration-100 hover:bg-brand-sub1 hover:text-neutral-0"
           onClick={onLogin}
