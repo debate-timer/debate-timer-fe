3be01ea573e2462bbcad2c2fc0cf06b5616fe814
Shawn Kang <csm06063@naver.com>
2025-07-02 18:07:16 +0900

    design: 로그인/로그아웃 버튼 디자인 및 위치 변경

--- src/layout/components/header/StickyTriSectionHeader.tsx
+++ src/layout/components/header/StickyTriSectionHeader.tsx
@@ -2,7 +2,6 @@ import { PropsWithChildren } from 'react';
 import { IoMdHome } from 'react-icons/io';
 import { useNavigate } from 'react-router-dom';
 import useLogout from '../../../hooks/mutations/useLogout';
-import { IoLogIn, IoLogOut } from 'react-icons/io5';
 import IconButton from '../../../components/IconButton/IconButton';
 import { isLoggedIn } from '../../../util/accessToken';
 import {
@@ -35,45 +34,54 @@ StickyTriSectionHeader.Center = function Center(props: PropsWithChildren) {
   return <div className="flex-1 items-center text-center">{children}</div>;
 };
 
-type HeaderIcons = 'home' | 'logout' | 'guest' | 'auth';
+type HeaderIcons = 'home';
 
 StickyTriSectionHeader.Right = function Right(props: PropsWithChildren) {
   const { children } = props;
   const navigate = useNavigate();
   const { mutate: logoutMutate } = useLogout(() => navigate('/home'));
   const { openModal, closeModal, ModalWrapper } = useModal({});
-  const defaultIcons: HeaderIcons[] = [];
-
-  if (isGuestFlow()) {
-    defaultIcons.push('guest');
-  }
-
-  if (isLoggedIn()) {
-    defaultIcons.push('home', 'auth');
-  } else {
-    defaultIcons.push('auth');
-  }
+  const defaultIcons: HeaderIcons[] = ['home'];
 
   return (
     <>
       <div className="flex flex-1 items-stretch justify-end gap-2 text-right">
-        {children && (
-          <>
-            {children}
-            <div className="w-[1px] self-stretch bg-neutral-500" />
-          </>
-        )}
+        {/* Auth related header items */}
+        <>
+          {/* Guest mode indicator */}
+          {!isGuestFlow() && (
+            <div className="animate-pulse rounded-full bg-neutral-300 px-4 py-2 font-semibold">
+              비회원 모드
+            </div>
+          )}
 
+          {/* Login and logout button */}
+          {isLoggedIn() && (
+            <button
+              className="rounded-full px-4 py-2 text-[20px] font-bold hover:bg-neutral-400"
+              onClick={() => logoutMutate()}
+            >
+              로그아웃
+            </button>
+          )}
+          {!isLoggedIn() && (
+            <button
+              className="rounded-full px-4 py-2 font-bold hover:bg-neutral-400"
+              onClick={() => openModal()}
+            >
+              로그인
+            </button>
+          )}
+
+          <div className="w-[1px] self-stretch bg-neutral-500" />
+        </>
+
+        {/* Buttons given as an argument */}
+        {children}
+
+        {/* Normal buttons */}
         {defaultIcons.map((iconName, index) => {
           switch (iconName) {
-            case 'guest':
-              return (
-                <div key={`${iconName}-${index}`}>
-                  <div className="animate-pulse rounded-full bg-neutral-300 px-4 py-2 font-semibold">
-                    비회원 모드
-                  </div>
-                </div>
-              );
             case 'home':
               return (
                 <div key={`${iconName}-${index}`}>
@@ -83,33 +91,11 @@ StickyTriSectionHeader.Right = function Right(props: PropsWithChildren) {
                       if (isGuestFlow()) {
                         deleteSessionCustomizeTableData();
                       }
-                      navigate('/');
+                      navigate('/home');
                     }}
                   />
                 </div>
               );
-            case 'auth':
-              if (isLoggedIn()) {
-                return (
-                  <div key={`${iconName}-${index}`}>
-                    <IconButton
-                      icon={<IoLogOut size={24} />}
-                      onClick={() => logoutMutate()}
-                      title="로그아웃"
-                    />
-                  </div>
-                );
-              } else {
-                return (
-                  <div key={`${iconName}-${index}`}>
-                    <IconButton
-                      icon={<IoLogIn size={24} />}
-                      onClick={() => openModal()}
-                      title="로그인"
-                    />
-                  </div>
-                );
-              }
             default:
               return null;
           }
