31335178e0a7c9052afa262402d1ee01dc018c4f
Shawn Kang <77564014+i-meant-to-be@users.noreply.github.com>
2025-07-07 22:56:43 +0900

    [DESIGN] 로그인/로그아웃 버튼 디자인 변경 (#300)
    * design: 로그인/로그아웃 버튼 디자인 및 위치 변경
    
    * chore: 주석 추가
    
    * feat: 홈 버튼 목적지 변경에 따라 LandingPage에 수정 사항 반영
    
    * refactor: CodeRabbit 제안 사항 반영
    
    * fix: Let Axios remove access token when it is expired
    
    * chore: 불필요한 주석 제거
    
    * refactor: Completely separated buisness logics from UI
    
    * refactor: Refactored functions with useCallback for optimization
    
    * refactor: Applied naming suggestions

--- src/layout/components/header/StickyTriSectionHeader.tsx
+++ src/layout/components/header/StickyTriSectionHeader.tsx
@@ -2,7 +2,6 @@ import { PropsWithChildren } from 'react';
 import { IoMdHome } from 'react-icons/io';
 import { useNavigate } from 'react-router-dom';
 import useLogout from '../../../hooks/mutations/useLogout';
-import { IoLogIn, IoLogOut } from 'react-icons/io5';
 import IconButton from '../../../components/IconButton/IconButton';
 import { isLoggedIn } from '../../../util/accessToken';
 import {
@@ -12,6 +11,9 @@ import {
 import { oAuthLogin } from '../../../util/googleAuth';
 import { useModal } from '../../../hooks/useModal';
 import DialogModal from '../../../components/DialogModal/DialogModal';
+
+// The type of header icons will be declared here.
+type HeaderIcons = 'home';
 
 function StickyTriSectionHeader(props: PropsWithChildren) {
   const { children } = props;
@@ -35,45 +37,52 @@ StickyTriSectionHeader.Center = function Center(props: PropsWithChildren) {
   return <div className="flex-1 items-center text-center">{children}</div>;
 };
 
-type HeaderIcons = 'home' | 'logout' | 'guest' | 'auth';
-
 StickyTriSectionHeader.Right = function Right(props: PropsWithChildren) {
-  const { children } = props;
+  const { children: buttons } = props;
   const navigate = useNavigate();
   const { mutate: logoutMutate } = useLogout(() => navigate('/home'));
   const { openModal, closeModal, ModalWrapper } = useModal({});
-  const defaultIcons: HeaderIcons[] = [];
-
-  if (isGuestFlow()) {
-    defaultIcons.push('guest');
-  }
-
-  if (isLoggedIn()) {
-    defaultIcons.push('home', 'auth');
-  } else {
-    defaultIcons.push('auth');
-  }
+  const defaultIcons: HeaderIcons[] = ['home']; // Icons that will be displayed on all pages are added here
 
   return (
     <>
       <div className="flex flex-1 items-stretch justify-end gap-2 text-right">
-        {children && (
-          <>
-            {children}
-            <div className="w-[1px] self-stretch bg-neutral-500" />
-          </>
-        )}
+        {/* Auth related header items */}
+        <>
+          {/* Guest mode indicator */}
+          {isGuestFlow() && (
+            <div className="animate-pulse rounded-full bg-neutral-300 px-4 py-2 font-semibold">
+              비회원 모드
+            </div>
+          )}
 
+          {/* Login and logout button */}
+          {isLoggedIn() && (
+            <button
+              className="rounded-full px-4 py-2 font-bold hover:bg-neutral-300"
+              onClick={() => logoutMutate()}
+            >
+              로그아웃
+            </button>
+          )}
+          {!isLoggedIn() && (
+            <button
+              className="rounded-full px-4 py-2 font-bold hover:bg-neutral-300"
+              onClick={() => openModal()}
+            >
+              로그인
+            </button>
+          )}
+
+          <div className="w-[1px] self-stretch bg-neutral-500" />
+        </>
+
+        {/* Buttons given as an argument */}
+        {buttons}
+
+        {/* Normal buttons */}
         {defaultIcons.map((iconName, index) => {
           switch (iconName) {
-            case 'guest':
-              return (
-                <div key={`${iconName}-${index}`}>
-                  <div className="animate-pulse rounded-full bg-neutral-300 px-4 py-2 font-semibold">
-                    비회원 모드
-                  </div>
-                </div>
-              );
             case 'home':
               return (
                 <div key={`${iconName}-${index}`}>
@@ -83,33 +92,11 @@ StickyTriSectionHeader.Right = function Right(props: PropsWithChildren) {
                       if (isGuestFlow()) {
                         deleteSessionCustomizeTableData();
                       }
-                      navigate('/');
+                      navigate('/home');
                     }}
                   />
                 </div>
               );
-            case 'auth':
-              if (isLoggedIn()) {
-                return (
-                  <div key={`${iconName}-${index}`}>
-                    <IconButton
-                      icon={<IoLogOut size={24} />}
-                      onClick={() => logoutMutate()}
-                      title="로그아웃"
-                    />
-                  </div>
-                );
-              } else {
-                return (
-                  <div key={`${iconName}-${index}`}>
-                    <IconButton
-                      icon={<IoLogIn size={24} />}
-                      onClick={() => openModal()}
-                      title="로그인"
-                    />
-                  </div>
-                );
-              }
             default:
               return null;
           }
--- src/page/LandingPage/LandingPage.tsx
+++ src/page/LandingPage/LandingPage.tsx
@@ -5,29 +5,29 @@ import TimerSection from './components/TimerSection';
 import TableSection from './components/TableSection';
 import ReviewSection from './components/ReviewSection';
 import ReportSection from './components/ReportSection';
-import { oAuthLogin } from '../../util/googleAuth';
-import { createTableShareUrl } from '../../util/arrayEncoding';
-import { SAMPLE_TABLE_DATA } from '../../constants/sample_table';
+import useLandingPageHandlers from './hooks/useLandingPageHandlers';
 
 export default function LandingPage() {
-  const handleStartWithoutLogin = () => {
-    // window.location.href = LANDING_URLS.START_WITHOUT_LOGIN_URL;
-    window.location.href = createTableShareUrl(
-      import.meta.env.VITE_SHARE_BASE_URL,
-      SAMPLE_TABLE_DATA,
-    );
-  };
+  const {
+    handleStartWithoutLogin,
+    handleDashboardButtonClick,
+    handleHeaderLoginButtonClick,
+    handleTableSectionLoginButtonClick,
+  } = useLandingPageHandlers();
 
   return (
     <div className="flex h-full w-full items-center justify-center bg-neutral-0">
       {/* 헤더 */}
-      <Header onLogin={() => oAuthLogin()} />
+      <Header onLoginButtonClicked={handleHeaderLoginButtonClick} />
 
       <main className="flex w-full flex-col items-center">
         {/* 흰색 배경 */}
         <div className="flex w-[95%] max-w-[1226px] flex-col gap-96 pb-48 pt-20 md:w-[64%]">
           {/* 메인 화면 */}
-          <MainSection onStartWithoutLogin={handleStartWithoutLogin} />
+          <MainSection
+            onStartWithoutLogin={handleStartWithoutLogin}
+            onDashboardButtonClicked={handleDashboardButtonClick}
+          />
           {/* 시간표 설정화면 */}
           <TimeTableSection />
         </div>
@@ -41,7 +41,7 @@ export default function LandingPage() {
         {/* 흰색 배경 */}
         <div className="flex w-[95%] max-w-[1226px] flex-col gap-96 py-48 md:w-[64%]">
           {/* 홈 설정 */}
-          <TableSection onLogin={() => oAuthLogin()} />
+          <TableSection onLogin={handleTableSectionLoginButtonClick} />
           {/* 리뷰 */}
           <ReviewSection onStartWithoutLogin={handleStartWithoutLogin} />
           {/* 버그 및 불편사항 제보 */}
