610f10d7fba18a97b42a91337f8394517cc0d1ad
jaeml06 <jaeml0630@gmail.com>
2025-07-09 15:28:37 +0900

    refactor: TiemrPage의 UI 컴포넌트 분리

--- src/page/TimerPage/TimerPage.tsx
+++ src/page/TimerPage/TimerPage.tsx
@@ -1,16 +1,14 @@
 import { useParams } from 'react-router-dom';
 import DefaultLayout from '../../layout/defaultLayout/DefaultLayout';
-import TimeBasedTimer from './components/TimeBasedTimer';
 import HeaderTableInfo from '../../components/HeaderTableInfo/HeaderTableInfo';
 import HeaderTitle from '../../components/HeaderTitle/HeaderTitle';
 import IconButton from '../../components/IconButton/IconButton';
 import { IoHelpCircle } from 'react-icons/io5';
-import { FaExchangeAlt } from 'react-icons/fa';
-import NormalTimer from './components/NormalTimer';
-import RoundControlButton from '../../components/RoundControlButton/RoundControlButton';
 import { bgColorMap, useTimerPageState } from './hooks/useTimerPageState';
 import { useTimerHotkey } from './hooks/useTimerHotkey';
 import useTimerPageModal from './hooks/useTimerPageModal';
+import RoundControlRow from './components/RoundControlRow';
+import TimerView from './components/TimerView';
 
 export default function TimerPage() {
   const pathParams = useParams();
@@ -25,21 +23,8 @@ export default function TimerPage() {
   const state = useTimerPageState(tableId);
 
   useTimerHotkey(state);
-  const {
-    warningBellRef,
-    finishBellRef,
-    data,
-    bg,
-    isAdditionalTimerAvailable,
-    index,
-    timer1,
-    timer2,
-    normalTimer,
-    prosConsSelected,
-    goToOtherItem,
-    switchCamp,
-    handleActivateTeam,
-  } = state;
+  const { warningBellRef, finishBellRef, data, bg, index, goToOtherItem } =
+    state;
 
   if (!data) {
     return null;
@@ -84,105 +69,15 @@ export default function TimerPage() {
             className={`flex h-full w-full flex-col items-center justify-center space-y-[25px] xl:space-y-[40px] ${bgColorMap[bg]}`}
           >
             {/* 타이머 두 개 + ENTER 버튼 */}
-            {data.table[index].boxType === 'NORMAL' && (
-              <NormalTimer
-                onStart={() => normalTimer.startTimer()}
-                onPause={() => normalTimer.pauseTimer()}
-                onReset={() => normalTimer.resetTimer()}
-                onSet={(second: number) => normalTimer.setTimer(second)}
-                isRunning={normalTimer.isRunning}
-                timer={normalTimer.timer ?? 0}
-                onChangeAdditionalTimer={
-                  normalTimer.handleChangeAdditionalTimer
-                }
-                isAdditionalTimerOn={normalTimer.isAdditionalTimerOn}
-                isAdditionalTimerAvailable={isAdditionalTimerAvailable}
-                item={data.table[index]}
-                teamName={
-                  data.table[index].stance === 'NEUTRAL'
-                    ? null
-                    : data.table[index].stance === 'PROS'
-                      ? data.info.prosTeamName
-                      : data.info.consTeamName
-                }
-              />
-            )}
-            {data.table[index].boxType === 'TIME_BASED' && (
-              <div className="relative flex flex-row items-center justify-center space-x-[30px]">
-                {/* 왼쪽 타이머 */}
-                <TimeBasedTimer
-                  onStart={() => timer1.startTimer()}
-                  onPause={() => timer1.pauseTimer()}
-                  onReset={() => timer1.resetCurrentTimer()}
-                  isRunning={timer1.isRunning}
-                  timer={timer1.totalTimer ?? 0}
-                  speakingTimer={timer1.speakingTimer}
-                  isSelected={prosConsSelected === 'pros'}
-                  onActivate={() => handleActivateTeam('pros')}
-                  prosCons="pros"
-                  teamName={data.info.prosTeamName}
-                />
-
-                {/* 오른쪽 타이머 */}
-                <TimeBasedTimer
-                  onStart={() => timer2.startTimer()}
-                  onPause={() => timer2.pauseTimer()}
-                  onReset={() => timer2.resetCurrentTimer()}
-                  isRunning={timer2.isRunning}
-                  timer={timer2.totalTimer ?? 0}
-                  speakingTimer={timer2.speakingTimer}
-                  isSelected={prosConsSelected === 'cons'}
-                  onActivate={() => handleActivateTeam('cons')}
-                  prosCons="cons"
-                  teamName={data.info.consTeamName}
-                />
-
-                {/* ENTER 버튼 */}
-                <button
-                  onClick={() => {
-                    switchCamp();
-                  }}
-                  className="absolute left-1/2 top-1/2 flex h-[78px] w-[78px] -translate-x-[70px] -translate-y-6 flex-col items-center justify-center rounded-full bg-neutral-600 text-white shadow-lg transition hover:bg-neutral-500 lg:h-[100px] lg:w-[100px] lg:-translate-x-20 lg:-translate-y-8"
-                >
-                  <FaExchangeAlt className="text-[28px] lg:text-[36px]" />
-                  <span className="text-[12px] font-semibold lg:text-[18px] lg:font-bold">
-                    ENTER
-                  </span>
-                </button>
-              </div>
-            )}
+            <TimerView state={state} />
             {/* Round control buttons on the bottom side */}
             {data && (
-              <div className="flex flex-row space-x-1 xl:space-x-8">
-                <div className="flex h-[70px] w-[175px] items-center justify-center lg:w-[200px]">
-                  {index === 0 && null}
-                  {index !== 0 && (
-                    <RoundControlButton
-                      type="PREV"
-                      onClick={() => {
-                        goToOtherItem(true);
-                      }}
-                    />
-                  )}
-                </div>
-
-                <div className="flex h-[70px] w-[175px] items-center justify-center lg:w-[200px]">
-                  {index === data.table.length - 1 && (
-                    <RoundControlButton
-                      type="DONE"
-                      onClick={openLoginAndStoreModalOrGoToOverviewPage}
-                    />
-                  )}
-                  {index !== data.table.length - 1 && (
-                    <RoundControlButton
-                      type="NEXT"
-                      onClick={() => {
-                        goToOtherItem(false);
-                      }}
-                    />
-                  )}
-                </div>
-              </div>
+              <RoundControlRow
+                table={data.table}
+                index={index}
+                goToOtherItem={goToOtherItem}
+                openDoneModal={openLoginAndStoreModalOrGoToOverviewPage}
+              />
             )}
           </div>
         </DefaultLayout.ContentContainer>
---
+++ src/page/TimerPage/components/TimerView.tsx
@@ -0,0 +1,72 @@
+// components/TimerView.tsx
+import { TimerPageLogics } from '../hooks/useTimerPageState';
+import NormalTimer from './NormalTimer';
+import TimeBasedTimer from './TimeBasedTimer';
+import { FaExchangeAlt } from 'react-icons/fa';
+
+export default function TimerView({ state }: { state: TimerPageLogics }) {
+  const {
+    data,
+    normalTimer,
+    timer1,
+    timer2,
+    prosConsSelected,
+    index,
+    isAdditionalTimerAvailable,
+    handleActivateTeam,
+    switchCamp,
+  } = state;
+  if (data?.table[index].boxType === 'NORMAL') {
+    return (
+      <>
+        <NormalTimer
+          normalTimerInstance={normalTimer}
+          isAdditionalTimerAvailable={isAdditionalTimerAvailable}
+          item={data.table[index]}
+          teamName={
+            data.table[index].stance === 'NEUTRAL'
+              ? null
+              : data.table[index].stance === 'PROS'
+                ? data.info.prosTeamName
+                : data.info.consTeamName
+          }
+        />
+      </>
+    );
+  }
+  if (data?.table[index].boxType === 'TIME_BASED') {
+    return (
+      <>
+        <div className="relative flex flex-row items-center justify-center space-x-[30px]">
+          {/* 왼쪽 타이머 */}
+          <TimeBasedTimer
+            timeBasedTimerInstance={timer1}
+            isSelected={prosConsSelected === 'pros'}
+            onActivate={() => handleActivateTeam('pros')}
+            prosCons="pros"
+            teamName={data.info.prosTeamName}
+          />
+          {/* 오른쪽 타이머 */}
+          <TimeBasedTimer
+            timeBasedTimerInstance={timer2}
+            isSelected={prosConsSelected === 'cons'}
+            onActivate={() => handleActivateTeam('cons')}
+            prosCons="cons"
+            teamName={data.info.consTeamName}
+          />
+          {/* ENTER 버튼 */}
+          <button
+            onClick={switchCamp}
+            className="absolute left-1/2 top-1/2 flex h-[78px] w-[78px] -translate-x-[70px] -translate-y-6 flex-col items-center justify-center rounded-full bg-neutral-600 text-white shadow-lg transition hover:bg-neutral-500 lg:h-[100px] lg:w-[100px] lg:-translate-x-20 lg:-translate-y-8"
+          >
+            <FaExchangeAlt className="text-[28px] lg:text-[36px]" />
+            <span className="text-[12px] font-semibold lg:text-[18px] lg:font-bold">
+              ENTER
+            </span>
+          </button>
+        </div>
+      </>
+    );
+  }
+  return null;
+}
