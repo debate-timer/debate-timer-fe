ae2d9657876b23cfc93465c4d20f4f39ba9b9b91
Shawn Kang <csm06063@naver.com>
2025-07-13 16:56:24 +0900

    test: Added story that covers too long title

--- src/page/TimerPage/stories/TimeBasedTimerTitle.stories.tsx
+++ src/page/TimerPage/stories/TimeBasedTimerTitle.stories.tsx
@@ -17,3 +17,15 @@ export const Default: Story = {
     width: 300,
   },
 };
+
+export const WhenTitleIsTooLong: Story = {
+  args: {
+    children: (
+      <h1 className="truncate text-ellipsis text-xl font-bold text-slate-800">
+        매우 엄청나게 완전 길어서 React 컴포넌트가 도저히 어떤 수를 써도 감당
+        불가능한 것처럼 보이는 대단하고 수려한 제목
+      </h1>
+    ),
+    width: 300,
+  },
+};
