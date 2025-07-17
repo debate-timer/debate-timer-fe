fe6f1141ba6daa2cba9d022a76a8e6b6af1f1107
Shawn Kang <csm06063@naver.com>
2025-07-17 23:43:20 +0900

    refactor: Replaced useQuery with useSuspenseQuery on GET all

--- src/page/TableListPage/TableListPage.tsx
+++ src/page/TableListPage/TableListPage.tsx
@@ -5,6 +5,8 @@ import DefaultLayout from '../../layout/defaultLayout/DefaultLayout';
 import { DebateTable } from '../../type/type';
 import Table from './components/Table';
 import HeaderTitle from '../../components/HeaderTitle/HeaderTitle';
+import { Suspense } from 'react';
+import LoadingIndicator from '../../components/async/LoadingIndicator';
 
 export default function TableListPage() {
   const { data } = useGetDebateTableList();
@@ -33,29 +35,31 @@ export default function TableListPage() {
       </DefaultLayout.Header>
 
       <DefaultLayout.ContentContainer>
-        <div className="flex max-w-[1140px] flex-wrap justify-start">
-          {/** Button that adds new table */}
-          <button
-            onClick={() => navigate(`/composition?mode=add`)}
-            className="m-5 h-[220px] w-[340px] rounded-[28px] bg-neutral-200 shadow-lg duration-200 hover:scale-105"
-          >
-            <h1 className="text-[100px] font-light text-neutral-500">+</h1>
-          </button>
+        <Suspense fallback={<LoadingIndicator />}>
+          <div className="flex max-w-[1140px] flex-wrap justify-start">
+            {/** Button that adds new table */}
+            <button
+              onClick={() => navigate(`/composition?mode=add`)}
+              className="m-5 h-[220px] w-[340px] rounded-[28px] bg-neutral-200 shadow-lg duration-200 hover:scale-105"
+            >
+              <h1 className="text-[100px] font-light text-neutral-500">+</h1>
+            </button>
 
-          {/** All tables */}
-          {data &&
-            data.tables.map((table: DebateTable, idx: number) => (
-              <Table
-                key={idx}
-                id={table.id}
-                name={table.name}
-                agenda={table.agenda}
-                onDelete={() => onDelete(table.id)}
-                onEdit={() => onEdit(table.id)}
-                onClick={() => onClick(table.id)}
-              />
-            ))}
-        </div>
+            {/** All tables */}
+            {data &&
+              data.tables.map((table: DebateTable, idx: number) => (
+                <Table
+                  key={idx}
+                  id={table.id}
+                  name={table.name}
+                  agenda={table.agenda}
+                  onDelete={() => onDelete(table.id)}
+                  onEdit={() => onEdit(table.id)}
+                  onClick={() => onClick(table.id)}
+                />
+              ))}
+          </div>
+        </Suspense>
       </DefaultLayout.ContentContainer>
     </DefaultLayout>
   );
