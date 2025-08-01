167d9872e209673ac9afdb6faf98e5fb743acd33
jaeml06 <jaeml0630@gmail.com>
2025-08-01 16:40:23 +0900

    fix: count가 음수일 경우, formatting처리 추가

--- src/util/formatting.ts
+++ src/util/formatting.ts
@@ -1,7 +1,8 @@
 export const Formatting = {
   formatSecondsToMinutes: (time: number) => {
-    const minutes = Math.floor(time / 60);
-    const seconds = time % 60;
+    const positiveTime = time >= 0 ? time : -time;
+    const minutes = Math.floor(positiveTime / 60);
+    const seconds = positiveTime % 60;
     return { minutes, seconds };
   },
   // Function that set all number to be have 2 digits (if time is 3, it will return 03)
