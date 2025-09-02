47ec0837074158a3dcd6d5f3ae054184f2ff8a9f
jaeml06 <jaeml0630@gmail.com>
2025-09-02 19:54:04 +0900

    refactor: 작전시간 -> 작전 시간 으로 변경

--- src/page/LandingPage/components/TamplateList.tsx
+++ src/page/LandingPage/components/TamplateList.tsx
@@ -9,7 +9,7 @@ export default function TamplateList({ data }: TamplateListProps) {
     <div
       className={[
         'grid gap-5',
-        // 작은 화면 1열, md에서 2열, xl에서 3열
+        // 2열, lg에서 3열
         'grid-cols-2 lg:grid-cols-3',
       ].join(' ')}
     >
