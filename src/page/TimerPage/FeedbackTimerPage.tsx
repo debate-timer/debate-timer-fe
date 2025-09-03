fe807919e82932ca755be70dfc5e77698b0346e5
useon <roqkftjs@gmail.com>
2025-09-03 23:27:00 +0900

    feat: 피드백 타이머 구현

---
+++ src/page/TimerPage/FeedbackTimerPage.tsx
@@ -0,0 +1,27 @@
+import { useEffect } from 'react';
+import { useFeedbackTimer } from './hooks/useFeedbackTimer';
+import FeedbackTimer from './components/FeedbackTimer';
+import DefaultLayout from '../../layout/defaultLayout/DefaultLayout';
+
+const INITIAL_TIME = 0;
+
+export default function FeedbackTimerPage() {
+  const feedbackTimerInstance = useFeedbackTimer();
+  const { setTimer, setDefaultTimer } = feedbackTimerInstance;
+
+  useEffect(() => {
+    // 페이지가 로드될 때 타이머의 초기 시간을 설정
+    setTimer(INITIAL_TIME);
+    setDefaultTimer(INITIAL_TIME);
+  }, [setTimer, setDefaultTimer]);
+
+  return (
+    <DefaultLayout>
+      <DefaultLayout.ContentContainer>
+        <div className="relative flex h-full w-full flex-col items-center justify-center space-y-[32px] pb-[66px] xl:space-y-[60px]">
+          <FeedbackTimer feedbackTimerInstance={feedbackTimerInstance} />
+        </div>
+      </DefaultLayout.ContentContainer>
+    </DefaultLayout>
+  );
+}
