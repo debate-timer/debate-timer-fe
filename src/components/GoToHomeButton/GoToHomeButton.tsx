642cff8dd1a0de3e42bc4fe7f90b712fd8b56913
useon <roqkftjs@gmail.com>
2025-09-06 23:15:10 +0900

    refactor: 홈으로 돌아가기 aria-label 추가

--- src/components/GoToHomeButton/GoToHomeButton.tsx
+++ src/components/GoToHomeButton/GoToHomeButton.tsx
@@ -9,6 +9,8 @@ export default function GoToHomeButton() {
 
   return (
     <button
+      type="button"
+      aria-label="홈으로 돌아가기"
       onClick={handleClick}
       className="flex h-[72px] w-[492px] items-center justify-center gap-[12px] rounded-full border-[2px] border-default-disabled/hover bg-default-white px-[16px] py-[11px] font-semibold text-default-black opacity-80 transition-colors duration-200 hover:bg-default-disabled/hover"
     >
