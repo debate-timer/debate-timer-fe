66066e563249334b37356920ee20bfdce5910444
jaeml06 <jaeml0630@gmail.com>
2025-10-08 13:13:14 +0900

    refactor: CheckBox 구현 방식 변경

--- src/page/VoteCompletePage/VoteCompletePage.tsx
+++ src/page/VoteCompletePage/VoteCompletePage.tsx
@@ -8,7 +8,7 @@ export default function VoteCompletePage() {
         <main className="flex min-h-screen items-center justify-center bg-white">
           <div className="flex flex-col items-center gap-4 text-center">
             {/* 체크 아이콘 배지 */}
-            <CheckBox color="#FECD4C" />
+            <CheckBox color="#FECD4C" checked={true} size={'4vw'} />
             {/* 완료 메시지 */}
             <h2 className="text-[min(max(1.25rem,3.5vw),3rem)] font-bold">
               투표가 완료되었습니다.
