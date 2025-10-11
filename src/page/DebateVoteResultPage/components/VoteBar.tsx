c951c520febc45cbe72d7cb20aa3db20bf2db6f8
jaeml06 <jaeml0630@gmail.com>
2025-10-11 22:14:06 +0900

    refactor: 파일 이름과 컴포넌트명 일치

--- src/page/DebateVoteResultPage/components/VoteBar.tsx
+++ src/page/DebateVoteResultPage/components/VoteBar.tsx
@@ -1,5 +1,5 @@
 import { motion } from 'framer-motion';
-import AnimatedCounter from './CountUp';
+import AnimatedCounter from './AnimatedCounter';
 import { TEAM_STYLE, TeamKey } from '../../../type/type';
 
 type VoteBarProps = {
