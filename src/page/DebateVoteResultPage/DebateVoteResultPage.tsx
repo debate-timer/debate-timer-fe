ba9b81577b24c769726536c57fe4f45dea72a116
jaeml06 <jaeml0630@gmail.com>
2025-10-11 23:22:10 +0900

    fix: pollId 조건문 오류 수정

--- src/page/DebateVoteResultPage/DebateVoteResultPage.tsx
+++ src/page/DebateVoteResultPage/DebateVoteResultPage.tsx
@@ -55,7 +55,7 @@ export default function DebateVoteResultPage() {
     }
   };
 
-  if (isValidPollId) {
+  if (!isValidPollId) {
     return (
       <DefaultLayout>
         <DefaultLayout.ContentContainer>
