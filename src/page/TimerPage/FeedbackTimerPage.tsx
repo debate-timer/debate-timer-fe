824ee2a72de324c4bfec001d6538d419c9c5696e
Shawn Kang <shineksu813@outlook.com>
2025-11-18 13:04:10 +0900

    feat: 피드백 타이머에 돌아가기 버튼 추가

--- src/page/TimerPage/FeedbackTimerPage.tsx
+++ src/page/TimerPage/FeedbackTimerPage.tsx
@@ -2,13 +2,22 @@ import { useEffect } from 'react';
 import { useFeedbackTimer } from './hooks/useFeedbackTimer';
 import FeedbackTimer from './components/FeedbackTimer';
 import DefaultLayout from '../../layout/defaultLayout/DefaultLayout';
-import GoToHomeButton from '../../components/GoToHomeButton/GoToHomeButton';
+import GoToDebateEndButton from '../../components/GoToDebateEndButton/GoToDebateEndButton';
+import { useParams } from 'react-router-dom';
 
 const INITIAL_TIME = 0;
 
 export default function FeedbackTimerPage() {
   const feedbackTimerInstance = useFeedbackTimer();
+  const { id } = useParams();
+  const rawTableId = Number(id);
   const { setTimer, setDefaultTimer } = feedbackTimerInstance;
+
+  // 테이블 ID 검증 로직
+  if (rawTableId === null || isNaN(Number(rawTableId))) {
+    throw new Error('테이블 ID가 올바르지 않습니다.');
+  }
+  const tableId = rawTableId ? Number(rawTableId) : 0;
 
   useEffect(() => {
     // 페이지가 로드될 때 타이머의 초기 시간을 설정
@@ -23,7 +32,7 @@ export default function FeedbackTimerPage() {
           <FeedbackTimer feedbackTimerInstance={feedbackTimerInstance} />
 
           <div className="fixed bottom-[8%] xl:bottom-[12%]">
-            <GoToHomeButton />
+            <GoToDebateEndButton tableId={tableId} />
           </div>
         </div>
       </DefaultLayout.ContentContainer>
