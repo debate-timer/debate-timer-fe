1ea28fa7058f0b1800db1a17c3eb5d040fc3b45d
Shawn Kang <77564014+i-meant-to-be@users.noreply.github.com>
2026-02-22 16:20:07 +0900

    [FEAT] 신 기능 소개 팝업 구현 (#411)
    * feat: 에셋 추가 및 구현
    
    * feat: 패치노트 데이터 타입 정의
    
    * feat: 패치노트 UI 구현
    
    * feat: 라우터에 패치노트 추가
    
    * refactor: 자세히 보기 버튼 로직 위치 변경
    
    * refactor: PatchNoteData 인터페이스 변수 타입 변경
    
    * chore: 자동 PR 세팅 스크립트에서 치코 제거
    
    * chore: 자동 PR 세팅 스크립트 제거
    
    * chore: 주석에 이미지가 필수임을 명시
    
    * test: Story 변수 값 수정
    
    * refactor: 불필요한 화살표 함수 제거
    
    * refactor: 로깅 함수 제거
    
    * feat: 모달 표시 여부를 따질 때 버전도 함께 확인
    
    * fix: isChecked가 최신 값으로 유지되지 못하는 문제 수정
    
    * design: 써니 피드백 반영
    
    * feat: 단일 이미지만 표시하는 패치 노트 타입 추가
    
    * feat: UI에 ImageOnlyPatchNoteData 변경 사항 반영
    
    * test: Story에 확장된 타입 명시
    
    * refactor: 체크박스에 속성 추가
    
    * refactor: 패치 노트 데이터 타입 다듬기
    
    * test: 누락된 매개변수 Story에 추가
    
    * refactor: 텍스트 클릭 시 체크박스도 터치되도록 개선
    
    * refactor: 체크박스 클릭 시 함수 별도로 분리
    
    * refactor: 매직 넘버 분리
    
    * feat: 디스플레이 크기에 따라 UI 크기도 바뀌도록 개선
    
    * feat: '자세히 보기' 버튼 클릭 시 모달 닫히도록 개선
    
    * fix: 파일 경로 수정
    
    * refactor: 필요 없는 타입 캐스팅 제거

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -18,6 +18,7 @@ import VoteParticipationPage from '../page/VoteParticipationPage/VoteParticipati
 import VoteCompletePage from '../page/VoteCompletePage/VoteCompletePage';
 import DebateVoteResultPage from '../page/DebateVoteResultPage/DebateVoteResultPage';
 import LanguageWrapper from './LanguageWrapper';
+import UpdateModalWrapper from '../components/UpdateModal/UpdateModalWrapper';
 
 const appRoutes = [
   {
@@ -108,6 +109,7 @@ const router = createBrowserRouter([
       <>
         <ErrorBoundaryWrapper />
         <BackActionHandler />
+        <UpdateModalWrapper />
       </>
     ),
     children: [
