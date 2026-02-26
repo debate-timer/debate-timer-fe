3338c23b36cd85dd2ff9a95cc0e6e5483b8864e2
Shawn Kang <csm06063@naver.com>
2026-02-26 16:06:38 +0900

    feat: 패치노트 모달이 랜딩 페이지, 시간표 목록 화면에만 표시되도록 수정

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -110,7 +110,6 @@ const router = createBrowserRouter(
         <>
           <ErrorBoundaryWrapper />
           <BackActionHandler />
-          <UpdateModalWrapper />
         </>
       ),
       children: [
