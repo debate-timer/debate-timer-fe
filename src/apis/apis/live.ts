0116e55389865584784ae87911a01caf1a351057
Shawn Kang <csm06063@naver.com>
2026-05-24 18:24:30 +0900

    feat: 화면 공유 로직 도메인 변경

--- src/apis/apis/live.ts
+++ src/apis/apis/live.ts
@@ -1,11 +1,11 @@
 import { request } from '../primitives';
 import { ApiUrl } from '../endpoints';
-import { GetChairmanTokenResponseType } from '../responses/share';
+import { GetChairmanTokenResponseType } from '../responses/live';
 
 export const getChairmanToken = (tableId: string) => {
   return request<GetChairmanTokenResponseType>(
     'GET',
-    `${ApiUrl.share}/${encodeURIComponent(tableId)}/chairman-token`,
+    `${ApiUrl.live}/${encodeURIComponent(tableId)}/chairman-token`,
     null,
     null,
   );
