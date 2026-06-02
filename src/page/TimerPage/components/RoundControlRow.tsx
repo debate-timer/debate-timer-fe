2358e639d99591f488f65064504d443a3978f59a
Shawn Kang <csm06063@naver.com>
2026-06-02 15:44:18 +0900

    refactor: 상태 관리 책임 변경에 따른 타이머 컴포넌트 변경

--- src/page/TimerPage/components/RoundControlRow.tsx
+++ src/page/TimerPage/components/RoundControlRow.tsx
@@ -1,31 +1,47 @@
 import clsx from 'clsx';
 import RoundControlButton from '../../../components/RoundControlButton/RoundControlButton';
 import { TimeBoxInfo } from '../../../type/type';
+import { SocketEventType } from '../../../apis/sockets/type';
 
 interface RoundControlRowProps {
   table: TimeBoxInfo[];
   index: number;
   goToOtherItem: (isPrev: boolean) => void;
   openDoneModal: () => void;
+  onEvent: (invoke: () => void, eventType: SocketEventType) => void;
   className?: string;
 }
 
 export default function RoundControlRow(props: RoundControlRowProps) {
-  const { table, index, goToOtherItem, openDoneModal, className = '' } = props;
+  const {
+    table,
+    index,
+    goToOtherItem,
+    openDoneModal,
+    onEvent,
+    className = '',
+  } = props;
+
   return (
     <div className={clsx('flex flex-row space-x-1 xl:space-x-8', className)}>
       <div className="flex w-[175px] items-center justify-center xl:w-[200px]">
         {index !== 0 && (
-          <RoundControlButton type="PREV" onClick={() => goToOtherItem(true)} />
+          <RoundControlButton
+            type="PREV"
+            onClick={() => onEvent(() => goToOtherItem(true), 'BEFORE')}
+          />
         )}
       </div>
       <div className="flex w-[175px] items-center justify-center xl:w-[200px]">
         {index === table.length - 1 ? (
-          <RoundControlButton type="DONE" onClick={openDoneModal} />
+          <RoundControlButton
+            type="DONE"
+            onClick={() => onEvent(openDoneModal, 'FINISHED')}
+          />
         ) : (
           <RoundControlButton
             type="NEXT"
-            onClick={() => goToOtherItem(false)}
+            onClick={() => onEvent(() => goToOtherItem(false), 'NEXT')}
           />
         )}
       </div>
--- src/page/TimerPage/components/TimerView.tsx
+++ src/page/TimerPage/components/TimerView.tsx
@@ -1,10 +1,17 @@
 // components/TimerView.tsx
+import { SocketEventType } from '../../../apis/sockets/type';
 import DTExchange from '../../../components/icons/Exchange';
 import { TimerPageLogics } from '../hooks/useTimerPageState';
 import NormalTimer from './NormalTimer';
 import TimeBasedTimer from './TimeBasedTimer';
 
-export default function TimerView({ state }: { state: TimerPageLogics }) {
+interface TimerViewProps {
+  state: TimerPageLogics;
+  onEvent: (invoke: () => void, eventType: SocketEventType) => void;
+}
+
+export default function TimerView({ state, onEvent }: TimerViewProps) {
+  // 상태 풀기
   const {
     data,
     normalTimer,
@@ -16,6 +23,8 @@ export default function TimerView({ state }: { state: TimerPageLogics }) {
     handleActivateTeam,
     switchCamp,
   } = state;
+
+  // 일반 타이머
   if (data && data.table[index].boxType === 'NORMAL') {
     return (
       <NormalTimer
@@ -25,9 +34,9 @@ export default function TimerView({ state }: { state: TimerPageLogics }) {
           isRunning: normalTimer.isRunning,
           handleChangeAdditionalTimer: normalTimer.handleChangeAdditionalTimer,
           handleCloseAdditionalTimer: normalTimer.handleCloseAdditionalTimer,
-          startTimer: normalTimer.startTimer,
-          pauseTimer: normalTimer.pauseTimer,
-          resetTimer: normalTimer.resetTimer,
+          startTimer: () => onEvent(normalTimer.startTimer, 'PLAY'),
+          pauseTimer: () => onEvent(normalTimer.pauseTimer, 'STOP'),
+          resetTimer: () => onEvent(normalTimer.resetTimer, 'RESET'),
           setTimer: normalTimer.setTimer,
         }}
         isAdditionalTimerAvailable={isAdditionalTimerAvailable}
@@ -42,6 +51,8 @@ export default function TimerView({ state }: { state: TimerPageLogics }) {
       />
     );
   }
+
+  // 자유 토론 타이머
   if (data && data.table[index].boxType === 'TIME_BASED') {
     return (
       <div className="flex flex-row items-center justify-center space-x-[30px]">
@@ -51,9 +62,10 @@ export default function TimerView({ state }: { state: TimerPageLogics }) {
             totalTimer: timer1.totalTimer,
             speakingTimer: timer1.speakingTimer,
             isRunning: timer1.isRunning,
-            startTimer: timer1.startTimer,
-            pauseTimer: timer1.pauseTimer,
-            resetCurrentTimer: () => timer1.resetCurrentTimer(timer2.isDone),
+            startTimer: () => onEvent(timer1.startTimer, 'PLAY'),
+            pauseTimer: () => onEvent(timer1.pauseTimer, 'STOP'),
+            resetCurrentTimer: () =>
+              onEvent(() => timer1.resetCurrentTimer(timer2.isDone), 'RESET'),
           }}
           item={data.table[index]}
           isSelected={prosConsSelected === 'PROS'}
@@ -64,7 +76,7 @@ export default function TimerView({ state }: { state: TimerPageLogics }) {
 
         {/* ENTER 버튼 */}
         <button
-          onClick={switchCamp}
+          onClick={() => onEvent(switchCamp, 'TEAM_SWITCH')}
           className="flex flex-col items-center justify-center rounded-[14px] bg-default-black2 px-[16px] py-[8px] text-default-white shadow-xl xl:px-[32px]"
         >
           <DTExchange className="size-[48px] xl:size-[64px]" />
@@ -77,9 +89,10 @@ export default function TimerView({ state }: { state: TimerPageLogics }) {
             totalTimer: timer2.totalTimer,
             speakingTimer: timer2.speakingTimer,
             isRunning: timer2.isRunning,
-            startTimer: timer2.startTimer,
-            pauseTimer: timer2.pauseTimer,
-            resetCurrentTimer: () => timer2.resetCurrentTimer(timer1.isDone),
+            startTimer: () => onEvent(timer2.startTimer, 'PLAY'),
+            pauseTimer: () => onEvent(timer2.pauseTimer, 'STOP'),
+            resetCurrentTimer: () =>
+              onEvent(() => timer2.resetCurrentTimer(timer1.isDone), 'RESET'),
           }}
           item={data.table[index]}
           isSelected={prosConsSelected === 'CONS'}
@@ -90,5 +103,6 @@ export default function TimerView({ state }: { state: TimerPageLogics }) {
       </div>
     );
   }
+
   return null;
 }
