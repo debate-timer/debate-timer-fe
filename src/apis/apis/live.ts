026fe4fcda9ffac41baaeef38641412ab63e986e
Shawn Kang <csm06063@naver.com>
2026-07-10 17:02:58 +0900

    feat: API 함수 및 테스트 코드 구현

--- src/apis/apis/live.ts
+++ src/apis/apis/live.ts
@@ -1,6 +1,9 @@
 import { request } from '../primitives';
 import { ApiUrl } from '../endpoints';
-import { GetChairmanTokenResponseType } from '../responses/live';
+import {
+  GetChairmanTokenResponseType,
+  GetDebateTableDataForShareResponseType,
+} from '../responses/live';
 
 export const getChairmanToken = (tableId: string) => {
   return request<GetChairmanTokenResponseType>(
@@ -10,3 +13,17 @@ export const getChairmanToken = (tableId: string) => {
     null,
   );
 };
+
+// GET /api/live/table/customize/{tableId}
+export async function getDebateTableDataForShare(
+  tableId: number,
+): Promise<GetDebateTableDataForShareResponseType> {
+  const response = await request<GetDebateTableDataForShareResponseType>(
+    'GET',
+    `${ApiUrl.live}/table/customize/${tableId}`,
+    null,
+    null,
+  );
+
+  return response.data;
+}
