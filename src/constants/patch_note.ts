2bb54aa798d2d088572566346183fa06fcd7cf17
Shawn Kang <77564014+i-meant-to-be@users.noreply.github.com>
2026-07-19 17:41:56 +0900

    [CHORE] 패치 노트 갱신 (#466)
    * chore: 에셋 추가
    
    * chore: 국제화 항목 추가
    
    * chore: 패치노트 갱신
    
    * refactor: 패치노트 모달 국제화 적용 및 일부 구조 개선
    
    * refactor: 국제화 적용에 따른 ModalWrapper 수정
    
    * feat: 닫기 버튼 추가

--- src/constants/patch_note.ts
+++ src/constants/patch_note.ts
@@ -1,18 +1,22 @@
 // 이미지 임포트는 아래와 같이
-import PatchNoteImage from '../assets/patchNote/0002.png';
+import PatchNoteImageKorean from '../assets/patchNote/0003_ko.png';
+import PatchNoteImageEnglish from '../assets/patchNote/0003_en.png';
 
 // 기본적인 패치 노트 인터페이스
 interface BasePatchNoteData {
   version: string; // 로컬 스토리지 키 관리를 위한 버전 (이 버전을 바꾸면 사용자의 '다시 보지 않기'가 초기화됨)
   link: string;
-  image: string;
+  imageKo: string;
+  imageEn: string;
 }
 
 // 사전 정의된 패치 노트 인터페이스
 export interface PredefinedPatchNoteData extends BasePatchNoteData {
   mode: 'predefined';
-  title: string;
-  description: string;
+  titleKo: string;
+  titleEn: string;
+  descriptionKo: string;
+  descriptionEn: string;
 }
 
 // 이미지만 존재하는 패치 노트 인터페이스
@@ -32,22 +36,35 @@ export function isPredefinedPatchNote(
 }
 
 // 현재 활성화된 업데이트 데이터 (이 부분만 수정해서 배포하면 됨)
-export const LATEST_PATCH_NOTE: PredefinedPatchNoteData = {
-  mode: 'predefined',
-  version: '0002',
-  title: '피드백 & 투표',
-  description:
-    '토론 종료 후 피드백 & 투표 기능으로 다양한 서비스를 이용하세요!',
-  image: PatchNoteImage,
-  link: 'https://bustling-bathtub-b3a.notion.site/2f41550c60cf80f69227e3145f6e19cc?pvs=143',
+export const LATEST_PATCH_NOTE: ImageOnlyPatchNoteData = {
+  mode: 'image-only',
+  version: '0003',
+  imageKo: PatchNoteImageKorean,
+  imageEn: PatchNoteImageEnglish,
+  link: 'https://bustling-bathtub-b3a.notion.site/2f51550c60cf8084ab0af4d1f35aeefd',
 };
 
 // ImageOnlyPatchNoteData의 예시
 /*
+export const PATCH_NOTE_0002: PredefinedPatchNoteData = {
+  mode: 'predefined',
+  version: '0002',
+  titleKo: '피드백 & 투표',
+  titleEn: 'Feedback & Voting',
+  descriptionKo:
+    '토론 종료 후 피드백 & 투표 기능으로 다양한 서비스를 이용하세요!',
+  descriptionEn:
+    'Use a variety of services with feedback and voting after the debate!',
+  imageKo: PatchNoteImageKorean,
+  imageEn: PatchNoteImageEnglish,
+  link: 'https://bustling-bathtub-b3a.notion.site/2f41550c60cf80f69227e3145f6e19cc?pvs=143',
+};
+
 export const TEST_PATCH_NOTE: ImageOnlyPatchNoteData = {
   mode: 'image-only',
   version: '0001',
-  image: PatchNoteImage,
+  imageKo: PatchNoteImageKorean,
+  imageEn: PatchNoteImageEnglish,
   link: 'https://bustling-bathtub-b3a.notion.site/2f41550c60cf80f69227e3145f6e19cc?pvs=143',
 };
 */
